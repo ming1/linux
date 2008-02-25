c11a571bd9cac0f8c6ad07fb17bf6d90b465d02c
Johannes Berg <johannes@sipsolutions.net>
2008-02-25 16:27:42 +0100

    mac80211: safely free beacon in ieee80211_if_reinit
    If ieee80211_if_reinit() is called from ieee80211_unregister_hw()
    then it is possible that the driver will still request a beacon
    (it is allowed to until ieee80211_unregister_hw() has returned.)
    This means we need to use an RCU-protected write to the beacon
    information even in this function.
    
    Signed-off-by: Johannes Berg <johannes@sipsolutions.net>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- net/mac80211/ieee80211_iface.c
+++ net/mac80211/ieee80211_iface.c
@@ -209,6 +209,7 @@ void ieee80211_if_reinit(struct net_device *dev)
 		/* Remove all virtual interfaces that use this BSS
 		 * as their sdata->bss */
 		struct ieee80211_sub_if_data *tsdata, *n;
+		struct beacon_data *beacon;
 
 		list_for_each_entry_safe(tsdata, n, &local->interfaces, list) {
 			if (tsdata != sdata && tsdata->bss == &sdata->u.ap) {
@@ -226,7 +227,10 @@ void ieee80211_if_reinit(struct net_device *dev)
 			}
 		}
 
-		kfree(sdata->u.ap.beacon);
+		beacon = sdata->u.ap.beacon;
+		rcu_assign_pointer(sdata->u.ap.beacon, NULL);
+		synchronize_rcu();
+		kfree(beacon);
 
 		while ((skb = skb_dequeue(&sdata->u.ap.ps_bc_buf))) {
 			local->total_ps_buffered--;
