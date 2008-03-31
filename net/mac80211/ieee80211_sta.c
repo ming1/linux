ac02e17fb86662a0c413507d79f9ef57e3206d47
Johannes Berg <johannes@sipsolutions.net>
2008-03-31 19:22:59 +0200

    mac80211 ibss: flush only stations belonging to current interface
    When joining a new IBSS, all old stations are flushed, but currently
    all stations belonging to all virtual interfaces are flushed, which
    is wrong. This patch fixes it.
    
    Signed-off-by: Johannes Berg <johannes@sipsolutions.net>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- net/mac80211/ieee80211_sta.c
+++ net/mac80211/ieee80211_sta.c
@@ -2252,8 +2252,10 @@ static int ieee80211_sta_join_ibss(struct net_device *dev,
 
 	sband = local->hw.wiphy->bands[local->hw.conf.channel->band];
 
+	sdata = IEEE80211_DEV_TO_SUB_IF(dev);
+
 	/* Remove possible STA entries from other IBSS networks. */
-	sta_info_flush(local, NULL);
+	sta_info_flush(local, sdata);
 
 	if (local->ops->reset_tsf) {
 		/* Reset own TSF to allow time synchronization work. */
@@ -2266,7 +2268,6 @@ static int ieee80211_sta_join_ibss(struct net_device *dev,
 
 	local->hw.conf.beacon_int = bss->beacon_int >= 10 ? bss->beacon_int : 10;
 
-	sdata = IEEE80211_DEV_TO_SUB_IF(dev);
 	sdata->drop_unencrypted = bss->capability &
 		WLAN_CAPABILITY_PRIVACY ? 1 : 0;
 
