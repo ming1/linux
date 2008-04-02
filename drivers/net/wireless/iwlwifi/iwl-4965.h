2598f0bcba0f42c7f3f60e233433d6b57e2bcf83
John W. Linville <linville@tuxdriver.com>
2008-04-01 20:51:46 -0400

    Revert "iwlwifi: remove IWL{4965,3945}_QOS"
    This reverts commit 4eab64618a9af024cff076b5dda037f70151787f.

--- drivers/net/wireless/iwlwifi/iwl-4965.h
+++ drivers/net/wireless/iwlwifi/iwl-4965.h
@@ -510,6 +510,8 @@ struct iwl_ht_info {
 };
 #endif				/*CONFIG_IWL4965_HT */
 
+#ifdef CONFIG_IWL4965_QOS
+
 union iwl4965_qos_capabity {
 	struct {
 		u8 edca_count:4;	/* bit 0-3 */
@@ -537,6 +539,7 @@ struct iwl4965_qos_info {
 	union iwl4965_qos_capabity qos_cap;
 	struct iwl4965_qosparam_cmd def_qos_parm;
 };
+#endif /*CONFIG_IWL4965_QOS */
 
 #define STA_PS_STATUS_WAKE             0
 #define STA_PS_STATUS_SLEEP            1
@@ -1170,7 +1173,9 @@ struct iwl4965_priv {
 	u16 assoc_capability;
 	u8 ps_mode;
 
+#ifdef CONFIG_IWL4965_QOS
 	struct iwl4965_qos_info qos_data;
+#endif /*CONFIG_IWL4965_QOS */
 
 	struct workqueue_struct *workqueue;
 
