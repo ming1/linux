f4a56f7e1d6105ba2c8deaf3aaa6e5da1eb5590e
John W. Linville <linville@tuxdriver.com>
2008-04-01 20:42:20 -0400

    Revert "iwlwifi: add struct iwl_cfg"
    This reverts commit 31b3a9c7a1cd5c3716573606530ae71f1e5eb756.

--- drivers/net/wireless/iwlwifi/iwl-4965.h
+++ drivers/net/wireless/iwlwifi/iwl-4965.h
@@ -967,7 +967,6 @@ struct iwl4965_priv {
 	struct ieee80211_hw *hw;
 	struct ieee80211_channel *ieee_channels;
 	struct ieee80211_rate *ieee_rates;
-	struct iwl_cfg *cfg;
 
 	/* temporary frame storage list */
 	struct list_head free_frames;
@@ -1127,6 +1126,7 @@ struct iwl4965_priv {
 	int is_open;
 
 	u8 mac80211_registered;
+	int is_abg;
 
 	u32 notif_missed_beacons;
 
