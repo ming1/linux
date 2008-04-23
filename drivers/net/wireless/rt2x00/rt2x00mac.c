1da8e5d47e7868015314c0a271cc8cb791f8d6c1
John W. Linville <linville@tuxdriver.com>
2008-04-23 13:43:06 -0400

    Revert "rt2x00: Rename config_preamble() to config_erp()"
    This reverts commit d793b48ec7f7e5d85922f546afe49fabf9de80bf.

--- drivers/net/wireless/rt2x00/rt2x00mac.c
+++ drivers/net/wireless/rt2x00/rt2x00mac.c
@@ -436,15 +436,17 @@ void rt2x00mac_bss_info_changed(struct ieee80211_hw *hw,
 	}
 
 	/*
-	 * When the erp information has changed, we should perform
-	 * additional configuration steps. For all other changes we are done.
+	 * When the preamble mode has changed, we should perform additional
+	 * configuration steps. For all other changes we are already done.
 	 */
-	if (changes & BSS_CHANGED_ERP_PREAMBLE)
-		rt2x00lib_config_erp(rt2x00dev, intf, bss_conf);
+	if (changes & BSS_CHANGED_ERP_PREAMBLE) {
+		rt2x00lib_config_preamble(rt2x00dev, intf,
+					  bss_conf->use_short_preamble);
 
-	spin_lock(&intf->lock);
-	memcpy(&intf->conf, bss_conf, sizeof(*bss_conf));
-	spin_unlock(&intf->lock);
+		spin_lock(&intf->lock);
+		memcpy(&intf->conf, bss_conf, sizeof(*bss_conf));
+		spin_unlock(&intf->lock);
+	}
 }
 EXPORT_SYMBOL_GPL(rt2x00mac_bss_info_changed);
 
