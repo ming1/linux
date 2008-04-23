326dd0780e3242f96e55ac3cb5b580d5719e77fc
John W. Linville <linville@tuxdriver.com>
2008-04-23 13:45:57 -0400

    Revert "rt2x00: correct address calc for queue private data"
    This reverts commit 042458eb79c573d01e9ed3f8db2a8c27ac37d075.

--- drivers/net/wireless/rt2x00/rt2x00queue.c
+++ drivers/net/wireless/rt2x00/rt2x00queue.c
@@ -160,8 +160,7 @@ static int rt2x00queue_alloc_entries(struct data_queue *queue,
 		return -ENOMEM;
 
 #define QUEUE_ENTRY_PRIV_OFFSET(__base, __index, __limit, __esize, __psize) \
-	( ((char *)(__base)) + ((__limit) * (__esize)) + \
-	    ((__index) * (__psize)) )
+	( (__base) + ((__limit) * (__esize)) + ((__index) * (__psize)) )
 
 	for (i = 0; i < queue->limit; i++) {
 		entries[i].flags = 0;
