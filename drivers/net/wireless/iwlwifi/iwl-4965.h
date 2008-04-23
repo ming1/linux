f899bc85a6565a3e3e142c076a2d3e6d50a8b17d
John W. Linville <linville@tuxdriver.com>
2008-04-23 13:42:30 -0400

    Revert "iwlwifi: Packing all 4965 parameters"
    This reverts commit 9a7d77fd6b58562be8c57a6c10321f728ac83011.

--- drivers/net/wireless/iwlwifi/iwl-4965.h
+++ drivers/net/wireless/iwlwifi/iwl-4965.h
@@ -67,7 +67,9 @@ extern struct pci_device_id iwl4965_hw_card_ids[];
 #define IWL_NOISE_MEAS_NOT_AVAILABLE (-127)
 
 /* Module parameters accessible from iwl-*.c */
-extern struct iwl_mod_params iwl4965_mod_params;
+extern int iwl4965_param_hwcrypto;
+extern int iwl4965_param_queues_num;
+extern int iwl4965_param_amsdu_size_8K;
 
 enum iwl4965_antenna {
 	IWL_ANTENNA_DIVERSITY,
