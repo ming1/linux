2e54c0f8f32dc5d3eb99ad30f5da5a8ae45692c3
Emmanuel Grumbach <emmanuel.grumbach@intel.com>
2008-04-24 11:55:20 -0700

    iwlwifi: move hw_rx_handler_setup to iwl-4965.c
    This patch moves hw_rx_handler_setup to iwl-4965.c
    
    Signed-off-by: Emmanuel Grumbach <emmanuel.grumbach@intel.com>
    Signed-off-by: Tomas Winkler <tomas.winkler@intel.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/iwlwifi/iwl4965-base.c
+++ drivers/net/wireless/iwlwifi/iwl4965-base.c
@@ -3246,7 +3246,7 @@ static void iwl4965_setup_rx_handlers(struct iwl_priv *priv)
 	priv->rx_handlers[REPLY_TX] = iwl4965_rx_reply_tx;
 
 	/* Set up hardware specific Rx handlers */
-	iwl4965_hw_rx_handler_setup(priv);
+	priv->cfg->ops->lib->rx_handler_setup(priv);
 }
 
 /**
