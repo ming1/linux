896866544937cc6503f86ac89a9bdb2488587d44
John W. Linville <linville@tuxdriver.com>
2008-06-07 14:09:55 -0400

    Revert "rt2x00: Remove ieee80211_tx_control argument from write_tx_desc()"
    This reverts commit 629c0b5fb2ea937104ebdf069876d5a91b8a1d1a.

--- drivers/net/wireless/rt2x00/rt2400pci.c
+++ drivers/net/wireless/rt2x00/rt2400pci.c
@@ -1001,7 +1001,8 @@ static int rt2400pci_set_device_state(struct rt2x00_dev *rt2x00dev,
  */
 static void rt2400pci_write_tx_desc(struct rt2x00_dev *rt2x00dev,
 				    struct sk_buff *skb,
-				    struct txentry_desc *txdesc)
+				    struct txentry_desc *txdesc,
+				    struct ieee80211_tx_control *control)
 {
 	struct skb_frame_desc *skbdesc = get_skb_frame_desc(skb);
 	__le32 *txd = skbdesc->desc;
@@ -1045,7 +1046,8 @@ static void rt2400pci_write_tx_desc(struct rt2x00_dev *rt2x00dev,
 			   test_bit(ENTRY_TXD_RTS_FRAME, &txdesc->flags));
 	rt2x00_set_field32(&word, TXD_W0_IFS, txdesc->ifs);
 	rt2x00_set_field32(&word, TXD_W0_RETRY_MODE,
-+			   test_bit(ENTRY_TXD_RETRY_MODE, &txdesc->flags));
+			   !!(control->flags &
+			      IEEE80211_TXCTL_LONG_RETRY_LIMIT));
 	rt2x00_desc_write(txd, 0, word);
 }
 
