4b9ea86962320d48cfd959898a4ffb7be3051a38
John W. Linville <linville@tuxdriver.com>
2008-06-13 17:15:34 -0400

    Revert "mac80211: make ieee80211_get_hdrlen_from_skb return unsigned"
    This reverts commit 92aad1bea7201f387155c1a175668a6d3e3e0758.

--- include/net/mac80211.h
+++ include/net/mac80211.h
@@ -1535,7 +1535,7 @@ ieee80211_get_buffered_bc(struct ieee80211_hw *hw, struct ieee80211_vif *vif);
  *
  * @skb: the frame
  */
-unsigned int ieee80211_get_hdrlen_from_skb(const struct sk_buff *skb);
+int ieee80211_get_hdrlen_from_skb(const struct sk_buff *skb);
 
 /**
  * ieee80211_get_hdrlen - get header length from frame control
