a059162d71446d70a665c697b17a90705bcd3d36
John W. Linville <linville@tuxdriver.com>
2008-06-13 17:25:02 -0400

    Revert "libertas: make some functions void"
    This reverts commit fb225a5f6e6ed5b002363e9db0eb42d7e110b383.

--- drivers/net/wireless/libertas/main.c
+++ drivers/net/wireless/libertas/main.c
@@ -890,7 +890,7 @@ int lbs_suspend(struct lbs_private *priv)
 }
 EXPORT_SYMBOL_GPL(lbs_suspend);
 
-void lbs_resume(struct lbs_private *priv)
+int lbs_resume(struct lbs_private *priv)
 {
 	lbs_deb_enter(LBS_DEB_FW);
 
@@ -906,6 +906,7 @@ void lbs_resume(struct lbs_private *priv)
 		netif_device_attach(priv->mesh_dev);
 
 	lbs_deb_leave(LBS_DEB_FW);
+	return 0;
 }
 EXPORT_SYMBOL_GPL(lbs_resume);
 
@@ -1155,7 +1156,7 @@ done:
 EXPORT_SYMBOL_GPL(lbs_add_card);
 
 
-void lbs_remove_card(struct lbs_private *priv)
+int lbs_remove_card(struct lbs_private *priv)
 {
 	struct net_device *dev = priv->dev;
 	union iwreq_data wrqu;
@@ -1190,6 +1191,7 @@ void lbs_remove_card(struct lbs_private *priv)
 	free_netdev(dev);
 
 	lbs_deb_leave(LBS_DEB_MAIN);
+	return 0;
 }
 EXPORT_SYMBOL_GPL(lbs_remove_card);
 
@@ -1260,9 +1262,10 @@ done:
 EXPORT_SYMBOL_GPL(lbs_start_card);
 
 
-void lbs_stop_card(struct lbs_private *priv)
+int lbs_stop_card(struct lbs_private *priv)
 {
 	struct net_device *dev = priv->dev;
+	int ret = -1;
 	struct cmd_ctrl_node *cmdnode;
 	unsigned long flags;
 
@@ -1287,7 +1290,8 @@ void lbs_stop_card(struct lbs_private *priv)
 
 	unregister_netdev(dev);
 
-	lbs_deb_leave(LBS_DEB_MAIN);
+	lbs_deb_leave_args(LBS_DEB_MAIN, "ret %d", ret);
+	return ret;
 }
 EXPORT_SYMBOL_GPL(lbs_stop_card);
 
