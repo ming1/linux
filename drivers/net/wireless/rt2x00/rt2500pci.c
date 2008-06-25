0ce5dfbe45b5665f5579a66304ca882f8baf9669
John W. Linville <linville@tuxdriver.com>
2008-06-25 16:47:37 -0400

    Revert "rt2x00: Remove ieee80211_tx_control argument from write_tx_desc()"
    This reverts commit dea5688367598b1446c2ec6e33f9b9c245c5b3ae.

--- drivers/net/wireless/rt2x00/rt2500pci.c
+++ drivers/net/wireless/rt2x00/rt2500pci.c
@@ -1155,7 +1155,8 @@ static int rt2500pci_set_device_state(struct rt2x00_dev *rt2x00dev,
  */
 static void rt2500pci_write_tx_desc(struct rt2x00_dev *rt2x00dev,
 				    struct sk_buff *skb,
-				    struct txentry_desc *txdesc)
+				    struct txentry_desc *txdesc,
+				    struct ieee80211_tx_control *control)
 {
 	struct skb_frame_desc *skbdesc = get_skb_frame_desc(skb);
 	__le32 *txd = skbdesc->desc;
@@ -1197,7 +1198,9 @@ static void rt2500pci_write_tx_desc(struct rt2x00_dev *rt2x00dev,
 	rt2x00_set_field32(&word, TXD_W0_CIPHER_OWNER, 1);
 	rt2x00_set_field32(&word, TXD_W0_IFS, txdesc->ifs);
 	rt2x00_set_field32(&word, TXD_W0_RETRY_MODE,
-			   test_bit(ENTRY_TXD_RETRY_MODE, &txdesc->flags));
+			   !!(control->flags &
+			      IEEE80211_TXCTL_LONG_RETRY_LIMIT));
+	rt2x00_set_field32(&word, TXD_W0_DATABYTE_COUNT, skbdesc->data_len);
 	rt2x00_set_field32(&word, TXD_W0_CIPHER_ALG, CIPHER_NONE);
 	rt2x00_desc_write(txd, 0, word);
 }
