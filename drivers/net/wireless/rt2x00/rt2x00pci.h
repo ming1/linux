39acabd4bc770c2298dbb711bc41cad827aac139
John W. Linville <linville@tuxdriver.com>
2008-07-16 11:26:59 -0400

    Revert "rt2x00: Fix kernel-doc"
    This reverts commit 818fcc5154a8777659a17ae0f44a5187cb99c0b8.

--- drivers/net/wireless/rt2x00/rt2x00pci.h
+++ drivers/net/wireless/rt2x00/rt2x00pci.h
@@ -98,9 +98,8 @@ int rt2x00pci_write_tx_data(struct rt2x00_dev *rt2x00dev,
  * struct queue_entry_priv_pci_rx: Per RX entry PCI specific information
  *
  * @desc: Pointer to device descriptor.
- * @desc_dma: DMA pointer to @desc.
  * @data: Pointer to device's entry memory.
- * @data_dma: DMA pointer to &data.
+ * @dma: DMA pointer to &data.
  */
 struct queue_entry_priv_pci_rx {
 	__le32 *desc;
@@ -114,9 +113,8 @@ struct queue_entry_priv_pci_rx {
  * struct queue_entry_priv_pci_tx: Per TX entry PCI specific information
  *
  * @desc: Pointer to device descriptor
- * @desc_dma: DMA pointer to @desc.
  * @data: Pointer to device's entry memory.
- * @data_dma: DMA pointer to &data.
+ * @dma: DMA pointer to &data.
  * @control: mac80211 control structure used to transmit data.
  */
 struct queue_entry_priv_pci_tx {
