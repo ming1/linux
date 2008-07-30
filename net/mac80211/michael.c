ae9b2beb4e5f68fddc00d4d9e70ad143bab4c33c
John W. Linville <linville@tuxdriver.com>
2008-07-30 17:09:39 -0400

    Revert "mac80211: michael.c use kernel-provided infrastructure"
    This reverts commit beb8d43a4afe701100d63480c48d79c4914dcd10.

--- net/mac80211/michael.c
+++ net/mac80211/michael.c
@@ -8,22 +8,54 @@
  */
 
 #include <linux/types.h>
-#include <linux/bitops.h>
-#include <asm/unaligned.h>
 
 #include "michael.h"
 
+static inline u32 rotr(u32 val, int bits)
+{
+	return (val >> bits) | (val << (32 - bits));
+}
+
+
+static inline u32 rotl(u32 val, int bits)
+{
+	return (val << bits) | (val >> (32 - bits));
+}
+
+
+static inline u32 xswap(u32 val)
+{
+	return ((val & 0xff00ff00) >> 8) | ((val & 0x00ff00ff) << 8);
+}
+
+
 #define michael_block(l, r) \
 do { \
-	r ^= rol32(l, 17); \
+	r ^= rotl(l, 17); \
 	l += r; \
-	r ^= ((l & 0xff00ff00) >> 8) | ((l & 0x00ff00ff) << 8); \
+	r ^= xswap(l); \
 	l += r; \
-	r ^= rol32(l, 3); \
+	r ^= rotl(l, 3); \
 	l += r; \
-	r ^= ror32(l, 2); \
+	r ^= rotr(l, 2); \
 	l += r; \
 } while (0)
+
+
+static inline u32 michael_get32(u8 *data)
+{
+	return data[0] | (data[1] << 8) | (data[2] << 16) | (data[3] << 24);
+}
+
+
+static inline void michael_put32(u32 val, u8 *data)
+{
+	data[0] = val & 0xff;
+	data[1] = (val >> 8) & 0xff;
+	data[2] = (val >> 16) & 0xff;
+	data[3] = (val >> 24) & 0xff;
+}
+
 
 void michael_mic(u8 *key, u8 *da, u8 *sa, u8 priority,
 		 u8 *data, size_t data_len, u8 *mic)
@@ -31,16 +63,16 @@ void michael_mic(u8 *key, u8 *da, u8 *sa, u8 priority,
 	u32 l, r, val;
 	size_t block, blocks, left;
 
-	l = get_unaligned_le32(key);
-	r = get_unaligned_le32(key + 4);
+	l = michael_get32(key);
+	r = michael_get32(key + 4);
 
 	/* A pseudo header (DA, SA, Priority, 0, 0, 0) is used in Michael MIC
 	 * calculation, but it is _not_ transmitted */
-	l ^= get_unaligned_le32(da);
+	l ^= michael_get32(da);
 	michael_block(l, r);
-	l ^= get_unaligned_le16(&da[4]) | (get_unaligned_le16(sa) << 16);
+	l ^= da[4] | (da[5] << 8) | (sa[0] << 16) | (sa[1] << 24);
 	michael_block(l, r);
-	l ^= get_unaligned_le32(&sa[2]);
+	l ^= michael_get32(&sa[2]);
 	michael_block(l, r);
 	l ^= priority;
 	michael_block(l, r);
@@ -50,7 +82,7 @@ void michael_mic(u8 *key, u8 *da, u8 *sa, u8 priority,
 	left = data_len % 4;
 
 	for (block = 0; block < blocks; block++) {
-		l ^= get_unaligned_le32(&data[block * 4]);
+		l ^= michael_get32(&data[block * 4]);
 		michael_block(l, r);
 	}
 
@@ -67,6 +99,6 @@ void michael_mic(u8 *key, u8 *da, u8 *sa, u8 priority,
 	/* last block is zero, so l ^ 0 = l */
 	michael_block(l, r);
 
-	put_unaligned_le32(l, mic);
-	put_unaligned_le32(r, mic + 4);
+	michael_put32(l, mic);
+	michael_put32(r, mic + 4);
 }
