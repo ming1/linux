9a72c7a1f65fd3da8678bea3cca24f2fcf342514
John W. Linville <linville@tuxdriver.com>
2008-08-06 14:24:14 -0400

    Revert "ipw2200 - Fix bad ipw_write8() macro"
    This reverts commit b51738e10d2db800be317b93efcf93e48c300600.

--- drivers/net/wireless/ipw2200.c
+++ drivers/net/wireless/ipw2200.c
@@ -305,10 +305,9 @@ static inline void ipw_write_reg32(struct ipw_priv *a, u32 b, u32 c)
 #define _ipw_write8(ipw, ofs, val) writeb((val), (ipw)->hw_base + (ofs))
 
 /* 8-bit direct write (for low 4K of SRAM/regs), with debug wrapper */
-#define ipw_write8(ipw, ofs, val) do { \
+#define ipw_write8(ipw, ofs, val) \
  IPW_DEBUG_IO("%s %d: write_direct8(0x%08X, 0x%08X)\n", __FILE__, __LINE__, (u32)(ofs), (u32)(val)); \
- _ipw_write8(ipw, ofs, val); \
- } while (0)
+ _ipw_write8(ipw, ofs, val)
 
 /* 16-bit direct write (low 4K) */
 #define _ipw_write16(ipw, ofs, val) writew((val), (ipw)->hw_base + (ofs))
