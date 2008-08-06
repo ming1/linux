1836c27c893602665d3b3f57bf8b139d8cd666e4
John W. Linville <linville@tuxdriver.com>
2008-08-06 14:24:42 -0400

    Revert "drivers/net/wireless/ipw2100.c: Release mutex in error handling code"
    This reverts commit 535d6d4dc33f5d519e6bc8e57b42021349230f55.

--- drivers/net/wireless/ipw2100.c
+++ drivers/net/wireless/ipw2100.c
@@ -6442,7 +6442,6 @@ static int ipw2100_resume(struct pci_dev *pci_dev)
 	if (err) {
 		printk(KERN_ERR "%s: pci_enable_device failed on resume\n",
 		       dev->name);
-		mutex_unlock(&priv->action_mutex);
 		return err;
 	}
 	pci_restore_state(pci_dev);
@@ -7147,7 +7146,7 @@ static int ipw2100_wx_get_rate(struct net_device *dev,
 	err = ipw2100_get_ordinal(priv, IPW_ORD_CURRENT_TX_RATE, &val, &len);
 	if (err) {
 		IPW_DEBUG_WX("failed querying ordinals.\n");
-		goto done;
+		return err;
 	}
 
 	switch (val & TX_RATE_MASK) {
