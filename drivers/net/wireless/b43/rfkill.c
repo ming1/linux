89352d7f60d7abd76e86ee55f0f97a02956d1508
John W. Linville <linville@tuxdriver.com>
2008-09-08 15:53:33 -0400

    Revert "b43: Implement dynamic PHY API"
    This reverts commit b00a4c233217d7a6469b96a8d33beba40061db17.

--- drivers/net/wireless/b43/rfkill.c
+++ drivers/net/wireless/b43/rfkill.c
@@ -24,7 +24,6 @@
 
 #include "rfkill.h"
 #include "b43.h"
-#include "phy_common.h"
 
 #include <linux/kmod.h>
 
@@ -115,11 +114,11 @@ static int b43_rfkill_soft_toggle(void *data, enum rfkill_state state)
 			goto out_unlock;
 		}
 		if (!dev->phy.radio_on)
-			b43_software_rfkill(dev, state);
+			b43_radio_turn_on(dev);
 		break;
 	case RFKILL_STATE_SOFT_BLOCKED:
 		if (dev->phy.radio_on)
-			b43_software_rfkill(dev, state);
+			b43_radio_turn_off(dev, 0);
 		break;
 	default:
 		b43warn(wl, "Received unexpected rfkill state %d.\n", state);
