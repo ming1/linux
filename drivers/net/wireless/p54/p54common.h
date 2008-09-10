b533dd5038747a1bb78d1adf891c5a67b8cae9e7
John W. Linville <linville@tuxdriver.com>
2008-09-10 15:27:20 -0400

    Revert "p54: fix rssi auto calibration"
    This reverts commit 68ef6177283fe79cf5a0b83d0a5cebd31f5156dd.

--- drivers/net/wireless/p54/p54common.h
+++ drivers/net/wireless/p54/p54common.h
@@ -89,16 +89,6 @@ struct pda_pa_curve_data_sample_rev1 {
 	u8 data_qpsk;
 	u8 data_16qam;
 	u8 data_64qam;
-} __attribute__ ((packed));
-
-struct p54_pa_curve_data_sample {
-	u8 rf_power;
-	u8 pa_detector;
-	u8 data_barker;
-	u8 data_bpsk;
-	u8 data_qpsk;
-	u8 data_16qam;
-	u8 data_64qam;
 	u8 padding;
 } __attribute__ ((packed));
 
@@ -222,8 +212,8 @@ struct p54_tx_control_filter {
 } __attribute__ ((packed));
 
 struct p54_tx_control_channel {
-	__le16 flags;
-	__le16 dwell;
+	__le16 magic1;
+	__le16 magic2;
 	u8 padding1[20];
 	struct pda_iq_autocal_entry iq_autocal;
 	u8 pa_points_per_curve;
@@ -232,13 +222,8 @@ struct p54_tx_control_channel {
 	u8 val_qpsk;
 	u8 val_16qam;
 	u8 val_64qam;
-	struct pda_pa_curve_data_sample_rev1 curve_data[8];
-	u8 dup_bpsk;
-	u8 dup_qpsk;
-	u8 dup_16qam;
-	u8 dup_64qam;
-	__le16 rssical_mul;
-	__le16 rssical_add;
+	struct pda_pa_curve_data_sample_rev1 curve_data[0];
+	/* additional padding/data after curve_data */
 } __attribute__ ((packed));
 
 struct p54_tx_control_led {
