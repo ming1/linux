a946ab4068447deca4b8dabbed1a7f087dd0a0d2
John W. Linville <linville@tuxdriver.com>
2008-09-23 15:28:43 -0400

    Revert "orinoco: Extend hermes_dld routines for Agere firmware"
    This reverts commit fc0c7df0307c91b8dc926139f153b747290ea698.

--- drivers/net/wireless/hermes_dld.h
+++ drivers/net/wireless/hermes_dld.h
@@ -27,8 +27,6 @@
 
 #include "hermes.h"
 
-int hermesi_program_init(hermes_t *hw, u32 offset);
-int hermesi_program_end(hermes_t *hw);
 int hermes_program(hermes_t *hw, const char *first_block, const char *end);
 
 int hermes_read_pda(hermes_t *hw,
@@ -39,9 +37,6 @@ int hermes_read_pda(hermes_t *hw,
 int hermes_apply_pda(hermes_t *hw,
 		     const char *first_pdr,
 		     const __le16 *pda);
-int hermes_apply_pda_with_defaults(hermes_t *hw,
-				   const char *first_pdr,
-				   const __le16 *pda);
 
 size_t hermes_blocks_length(const char *first_block);
 
