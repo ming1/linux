8148bf3b30cdf02ba09fb6028258fd6ba2cea46a
John W. Linville <linville@tuxdriver.com>
2008-09-30 15:00:39 -0400

    Revert "ath9k: connectivity is lost after Group rekeying is done"
    This reverts commit 7ca85b5c607fa143e8ae949be8650bc8c7f25e48.

--- drivers/net/wireless/ath9k/xmit.c
+++ drivers/net/wireless/ath9k/xmit.c
@@ -315,11 +315,11 @@ static int ath_tx_prepare(struct ath_softc *sc,
 		txctl->keyix = tx_info->control.hw_key->hw_key_idx;
 		txctl->frmlen += tx_info->control.icv_len;
 
-		if (tx_info->control.hw_key->alg == ALG_WEP)
+		if (sc->sc_keytype == ATH9K_CIPHER_WEP)
 			txctl->keytype = ATH9K_KEY_TYPE_WEP;
-		else if (tx_info->control.hw_key->alg == ALG_TKIP)
+		else if (sc->sc_keytype == ATH9K_CIPHER_TKIP)
 			txctl->keytype = ATH9K_KEY_TYPE_TKIP;
-		else if (tx_info->control.hw_key->alg == ALG_CCMP)
+		else if (sc->sc_keytype == ATH9K_CIPHER_AES_CCM)
 			txctl->keytype = ATH9K_KEY_TYPE_AES;
 	}
 
