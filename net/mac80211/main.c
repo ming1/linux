2a218b5214469fcfbde9a520992b4965dd1db208
John W. Linville <linville@tuxdriver.com>
2008-10-10 17:58:29 -0400

    Revert "mac80211: warn on some invalid vlan operations"
    This reverts commit d2cd1e95156d53958e5e8f4d7f8c24fb32c05f4d.

--- net/mac80211/main.c
+++ net/mac80211/main.c
@@ -156,9 +156,6 @@ int ieee80211_if_config(struct ieee80211_sub_if_data *sdata, u32 changed)
 	if (WARN_ON(!netif_running(sdata->dev)))
 		return 0;
 
-	if (WARN_ON(sdata->vif.type == NL80211_IFTYPE_AP_VLAN))
-		return -EINVAL;
-
 	if (!local->ops->config_interface)
 		return 0;
 
@@ -323,9 +320,6 @@ void ieee80211_bss_info_change_notify(struct ieee80211_sub_if_data *sdata,
 				      u32 changed)
 {
 	struct ieee80211_local *local = sdata->local;
-
-	if (WARN_ON(sdata->vif.type == NL80211_IFTYPE_AP_VLAN))
-		return;
 
 	if (!changed)
 		return;
