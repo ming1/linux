80abe4e19ed1f9b11ed1dbb7afac45f256859120
John W. Linville <linville@tuxdriver.com>
2008-10-24 17:08:21 -0400

    Revert "rfkill: use strict_strtoul (v2)"
    This reverts commit d057d9ee9fff5f65fcbc13d3386a2d7fef8da935.

--- net/rfkill/rfkill.c
+++ net/rfkill/rfkill.c
@@ -402,15 +402,11 @@ static ssize_t rfkill_state_store(struct device *dev,
 				  const char *buf, size_t count)
 {
 	struct rfkill *rfkill = to_rfkill(dev);
-	unsigned long state;
+	unsigned int state = simple_strtoul(buf, NULL, 0);
 	int error;
 
 	if (!capable(CAP_NET_ADMIN))
 		return -EPERM;
-
-	error = strict_strtoul(buf, 0, &state);
-	if (error)
-		return error;
 
 	/* RFKILL_STATE_HARD_BLOCKED is illegal here... */
 	if (state != RFKILL_STATE_UNBLOCKED &&
@@ -439,8 +435,7 @@ static ssize_t rfkill_claim_store(struct device *dev,
 				  const char *buf, size_t count)
 {
 	struct rfkill *rfkill = to_rfkill(dev);
-	unsigned long claim_tmp;
-	bool claim;
+	bool claim = !!simple_strtoul(buf, NULL, 0);
 	int error;
 
 	if (!capable(CAP_NET_ADMIN))
@@ -448,11 +443,6 @@ static ssize_t rfkill_claim_store(struct device *dev,
 
 	if (rfkill->user_claim_unsupported)
 		return -EOPNOTSUPP;
-
-	error = strict_strtoul(buf, 0, &claim_tmp);
-	if (error)
-		return error;
-	claim = !!claim_tmp;
 
 	/*
 	 * Take the global lock to make sure the kernel is not in
