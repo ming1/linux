2e9e5ae58e3c6b4e604c9f53a449b5bca2ed45d6
John W. Linville <linville@tuxdriver.com>
2008-11-21 14:25:32 -0500

    Revert "p54: eliminate warning for uninitialized variable 'tim_len'"
    This reverts commit a17cc841f6db7cbf7c323929e59ad37d29ac1cbf.

--- drivers/net/wireless/p54/p54common.c
+++ drivers/net/wireless/p54/p54common.c
@@ -1055,7 +1055,7 @@ static int p54_tx(struct ieee80211_hw *dev, struct sk_buff *skb)
 	struct p54_common *priv = dev->priv;
 	struct p54_hdr *hdr;
 	struct p54_tx_data *txhdr;
-	size_t padding, len, tim_len = 0;
+	size_t padding, len, tim_len;
 	int i, j, ridx;
 	u16 hdr_flags = 0, aid = 0;
 	u8 rate, queue;
