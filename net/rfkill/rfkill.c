e9c466d0be80d0761c4e6349ac71956d2cb770ba
John W. Linville <linville@tuxdriver.com>
2008-12-11 14:54:28 -0500

    Revert "rfkill: honour EPO state when resuming a rfkill controller"
    This reverts commit 518f6a13414f98f9955a58d92b35e02e5002c771.

--- net/rfkill/rfkill.c
+++ net/rfkill/rfkill.c
@@ -581,17 +581,8 @@ static int rfkill_resume(struct device *dev)
 
 		dev->power.power_state.event = PM_EVENT_ON;
 
-		/*
-		 * If we are under EPO, kick transmitter offline,
-		 * otherwise restore to pre-suspend state.
-		 *
-		 * Issue a notification in any case
-		 */
-		rfkill_toggle_radio(rfkill,
-				rfkill_epo_lock_active ?
-					RFKILL_STATE_SOFT_BLOCKED :
-					rfkill->state,
-				1);
+		/* restore radio state AND notify everybody */
+		rfkill_toggle_radio(rfkill, rfkill->state, 1);
 
 		mutex_unlock(&rfkill->mutex);
 	}
