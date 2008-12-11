2eced2a8bbd2ef48000b8d2b70a486a3b4d37cdc
Rami Rosen <ramirose@gmail.com>
2008-12-11 14:00:25 +0200

    mac80211: misc cleanups
    This patch removes unneeded member (skbuff) from
    ieee80211_ibss_add_sta() method in its declaration (in ieee80211_i.h)
    and its callers (in rx.c and mlme.c)
    
    This patch removes unneeded member from struct ieee80211_rx_data
    in ieee80211_i.h.
    
    (Originally posted as two patches. -- JWL)
    
    Signed-off-by: Rami Rosen <ramirose@gmail.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- net/mac80211/mlme.c
+++ net/mac80211/mlme.c
@@ -1580,8 +1580,7 @@ static void ieee80211_rx_bss_info(struct ieee80211_sub_if_data *sdata,
 				    (unsigned long long) sta->sta.supp_rates[band]);
 #endif
 		} else {
-			ieee80211_ibss_add_sta(sdata, NULL, mgmt->bssid,
-					       mgmt->sa, supp_rates);
+			ieee80211_ibss_add_sta(sdata, mgmt->bssid, mgmt->sa, supp_rates);
 		}
 
 		rcu_read_unlock();
@@ -1654,9 +1653,7 @@ static void ieee80211_rx_bss_info(struct ieee80211_sub_if_data *sdata,
 			       sdata->dev->name, print_mac(mac, mgmt->bssid));
 #endif
 			ieee80211_sta_join_ibss(sdata, &sdata->u.sta, bss);
-			ieee80211_ibss_add_sta(sdata, NULL,
-					       mgmt->bssid, mgmt->sa,
-					       supp_rates);
+			ieee80211_ibss_add_sta(sdata, mgmt->bssid, mgmt->sa, supp_rates);
 		}
 	}
 
@@ -2399,8 +2396,7 @@ void ieee80211_sta_setup_sdata(struct ieee80211_sub_if_data *sdata)
  * must be callable in atomic context.
  */
 struct sta_info *ieee80211_ibss_add_sta(struct ieee80211_sub_if_data *sdata,
-					struct sk_buff *skb, u8 *bssid,
-					u8 *addr, u64 supp_rates)
+					u8 *bssid,u8 *addr, u64 supp_rates)
 {
 	struct ieee80211_local *local = sdata->local;
 	struct sta_info *sta;
