05198e61c72ca2361b77194e6ec75e395fb44652
John W. Linville <linville@tuxdriver.com>
2008-12-11 14:52:51 -0500

    Revert "wireless: use individual buffers for printing ssid values"
    This reverts commit 2131749cabbda21d538d55ad28cc148de93a4f6d.

--- drivers/net/wireless/libertas/debugfs.c
+++ drivers/net/wireless/libertas/debugfs.c
@@ -67,7 +67,6 @@ static ssize_t lbs_getscantable(struct file *file, char __user *userbuf,
 	unsigned long addr = get_zeroed_page(GFP_KERNEL);
 	char *buf = (char *)addr;
 	DECLARE_MAC_BUF(mac);
-	DECLARE_SSID_BUF(ssid);
 	struct bss_descriptor * iter_bss;
 
 	pos += snprintf(buf+pos, len-pos,
@@ -89,8 +88,7 @@ static ssize_t lbs_getscantable(struct file *file, char __user *userbuf,
 				spectrum_mgmt ? 'S' : ' ');
 		pos += snprintf(buf+pos, len-pos, " %04d |", SCAN_RSSI(iter_bss->rssi));
 		pos += snprintf(buf+pos, len-pos, " %s\n",
-		                print_ssid(ssid, iter_bss->ssid,
-					   iter_bss->ssid_len));
+		                escape_ssid(iter_bss->ssid, iter_bss->ssid_len));
 
 		numscansdone++;
 	}
