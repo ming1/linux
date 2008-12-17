c743e91c3e2d7146efb31d16561123556ce763d2
Zhu Yi <yi.zhu@intel.com>
2008-12-17 16:52:33 +0800

    iwlwifi: use GFP_KERNEL to allocate Rx SKB memory
    Previously we allocate Rx SKB with GFP_ATOMIC flag. This is because we need
    to hold a spinlock to protect the two rx_used and rx_free lists operation
    in the rxq.
    
    	spin_lock();
    	...
    	element = rxq->rx_used.next;
    	element->skb = alloc_skb(..., GFP_ATOMIC);
    	list_del(element);
    	list_add_tail(&element->list, &rxq->rx_free);
    	...
    	spin_unlock();
    
    After spliting the rx_used delete and rx_free insert into two operations,
    we don't require the skb allocation in an atomic context any more (the
    function itself is scheduled in a workqueue).
    
    	spin_lock();
    	...
    	element = rxq->rx_used.next;
    	list_del(element);
    	...
    	spin_unlock();
    	...
    	element->skb = alloc_skb(..., GFP_KERNEL);
    	...
    	spin_lock()
    	...
    	list_add_tail(&element->list, &rxq->rx_free);
    	...
    	spin_unlock();
    
    This patch should fix the "iwlagn: Can not allocate SKB buffers" warning
    we see recently.
    
    Signed-off-by: Zhu Yi <yi.zhu@intel.com>
    Acked-by: Tomas Winkler <tomas.winkler@intel.com>
    Cc: stable@kernel.org
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/iwlwifi/iwl-agn.c
+++ drivers/net/wireless/iwlwifi/iwl-agn.c
@@ -1111,16 +1111,6 @@ static void iwl_setup_rx_handlers(struct iwl_priv *priv)
 	priv->cfg->ops->lib->rx_handler_setup(priv);
 }
 
-/*
- * this should be called while priv->lock is locked
-*/
-static void __iwl_rx_replenish(struct iwl_priv *priv)
-{
-	iwl_rx_allocate(priv);
-	iwl_rx_queue_restock(priv);
-}
-
-
 /**
  * iwl_rx_handle - Main entry function for receiving responses from uCode
  *
@@ -1229,7 +1219,7 @@ void iwl_rx_handle(struct iwl_priv *priv)
 			count++;
 			if (count >= 8) {
 				priv->rxq.read = i;
-				__iwl_rx_replenish(priv);
+				iwl_rx_queue_restock(priv);
 				count = 0;
 			}
 		}
