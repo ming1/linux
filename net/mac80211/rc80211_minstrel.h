71529430730006e63ebbdad3fa64b0594723304d
John W. Linville <linville@tuxdriver.com>
2008-12-19 14:01:20 -0500

    Revert "minstrel: improve performance for non-MRR drivers"
    This reverts commit f88eef122caad1cc72e7fa29f9bd34624300c4ed.

--- net/mac80211/rc80211_minstrel.h
+++ net/mac80211/rc80211_minstrel.h
@@ -16,7 +16,6 @@ struct minstrel_rate {
 	unsigned int perfect_tx_time;
 	unsigned int ack_time;
 
-	int sample_limit;
 	unsigned int retry_count;
 	unsigned int retry_count_cts;
 	unsigned int retry_count_rtscts;
@@ -58,7 +57,6 @@ struct minstrel_sta_info {
 
 	int n_rates;
 	struct minstrel_rate *r;
-	bool prev_sample;
 
 	/* sampling table */
 	u8 *sample_table;
