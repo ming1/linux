198f9542ec9e0b0a9f39d09ef47788aad4670554
John W. Linville <linville@tuxdriver.com>
2008-12-19 13:51:54 -0500

    Revert "mac80211: Fix HT channel selection"
    This reverts commit cc235b925ec326dd47b59840714e4d16bf1b3bb8.

--- net/mac80211/main.c
+++ net/mac80211/main.c
@@ -195,30 +195,37 @@ int ieee80211_hw_config(struct ieee80211_local *local, u32 changed)
 	struct ieee80211_channel *chan;
 	int ret = 0;
 	int power;
-	enum nl80211_channel_type channel_type;
+	enum nl80211_sec_chan_offset sec_chan_offset;
 
 	might_sleep();
 
 	if (local->sw_scanning) {
 		chan = local->scan_channel;
-		channel_type = NL80211_CHAN_NO_HT;
+		sec_chan_offset = NL80211_SEC_CHAN_NO_HT;
 	} else {
 		chan = local->oper_channel;
-		channel_type = local->oper_channel_type;
+		sec_chan_offset = local->oper_sec_chan_offset;
 	}
 
 	if (chan != local->hw.conf.channel ||
-	    channel_type != local->hw.conf.ht.channel_type) {
+	    sec_chan_offset != local->hw.conf.ht.sec_chan_offset) {
 		local->hw.conf.channel = chan;
-		local->hw.conf.ht.channel_type = channel_type;
-		switch (channel_type) {
-		case NL80211_CHAN_NO_HT:
+		switch (sec_chan_offset) {
+		case NL80211_SEC_CHAN_NO_HT:
 			local->hw.conf.ht.enabled = false;
+			local->hw.conf.ht.sec_chan_offset = 0;
 			break;
-		case NL80211_CHAN_HT20:
-		case NL80211_CHAN_HT40MINUS:
-		case NL80211_CHAN_HT40PLUS:
+		case NL80211_SEC_CHAN_DISABLED:
 			local->hw.conf.ht.enabled = true;
+			local->hw.conf.ht.sec_chan_offset = 0;
+			break;
+		case NL80211_SEC_CHAN_BELOW:
+			local->hw.conf.ht.enabled = true;
+			local->hw.conf.ht.sec_chan_offset = -1;
+			break;
+		case NL80211_SEC_CHAN_ABOVE:
+			local->hw.conf.ht.enabled = true;
+			local->hw.conf.ht.sec_chan_offset = 1;
 			break;
 		}
 		changed |= IEEE80211_CONF_CHANGE_CHANNEL;
