8e587505859d8e4b86e0acbf7399de6166d49015
John W. Linville <linville@tuxdriver.com>
2008-12-19 13:54:59 -0500

    Revert "ath9k: Handle -ENOMEM on RX gracefully"
    This reverts commit ddbe9f42f19f9b472da79312812186240bd13c43.

--- drivers/net/wireless/ath9k/recv.c
+++ drivers/net/wireless/ath9k/recv.c
@@ -90,6 +90,29 @@ static struct sk_buff *ath_rxbuf_alloc(struct ath_softc *sc, u32 len)
 	return skb;
 }
 
+static void ath_rx_requeue(struct ath_softc *sc, struct ath_buf *bf)
+{
+	struct sk_buff *skb;
+
+	ASSERT(bf != NULL);
+
+	if (bf->bf_mpdu == NULL) {
+		skb = ath_rxbuf_alloc(sc, sc->sc_rxbufsize);
+		if (skb != NULL) {
+			bf->bf_mpdu = skb;
+			bf->bf_buf_addr = pci_map_single(sc->pdev, skb->data,
+							 sc->sc_rxbufsize,
+							 PCI_DMA_FROMDEVICE);
+			bf->bf_dmacontext = bf->bf_buf_addr;
+
+		}
+	}
+
+	list_move_tail(&bf->list, &sc->sc_rxbuf);
+	ath_rx_buf_link(sc, bf);
+}
+
+
 static int ath_rate2idx(struct ath_softc *sc, int rate)
 {
 	int i = 0, cur_band, n_rates;
@@ -431,7 +454,7 @@ int ath_rx_tasklet(struct ath_softc *sc, int flush)
 
 	struct ath_buf *bf;
 	struct ath_desc *ds;
-	struct sk_buff *skb = NULL, *requeue_skb;
+	struct sk_buff *skb = NULL;
 	struct ieee80211_rx_status rx_status;
 	struct ath_hal *ah = sc->sc_ah;
 	struct ieee80211_hdr *hdr;
@@ -508,28 +531,17 @@ int ath_rx_tasklet(struct ath_softc *sc, int flush)
 		 * chain it back at the queue without processing it.
 		 */
 		if (flush)
-			goto requeue;
+			goto rx_next;
 
 		if (!ds->ds_rxstat.rs_datalen)
-			goto requeue;
+			goto rx_next;
 
 		/* The status portion of the descriptor could get corrupted. */
 		if (sc->sc_rxbufsize < ds->ds_rxstat.rs_datalen)
-			goto requeue;
+			goto rx_next;
 
 		if (!ath_rx_prepare(skb, ds, &rx_status, &decrypt_error, sc))
-			goto requeue;
-
-		/* Ensure we always have an skb to requeue once we are done
-		 * processing the current buffer's skb */
-		requeue_skb = ath_rxbuf_alloc(sc, sc->sc_rxbufsize);
-
-		/* If there is no memory we ignore the current RX'd frame,
-		 * tell hardware it can give us a new frame using the old
-		 * skb and put it at the tail of the sc->sc_rxbuf list for
-		 * processing. */
-		if (!requeue_skb)
-			goto requeue;
+			goto rx_next;
 
 		/* Sync and unmap the frame */
 		pci_dma_sync_single_for_cpu(sc->pdev, bf->bf_buf_addr,
@@ -566,13 +578,7 @@ int ath_rx_tasklet(struct ath_softc *sc, int flush)
 
 		/* Send the frame to mac80211 */
 		__ieee80211_rx(sc->hw, skb, &rx_status);
-
-		/* We will now give hardware our shiny new allocated skb */
-		bf->bf_mpdu = requeue_skb;
-		bf->bf_buf_addr = pci_map_single(sc->pdev, requeue_skb->data,
-					 sc->sc_rxbufsize,
-					 PCI_DMA_FROMDEVICE);
-		bf->bf_dmacontext = bf->bf_buf_addr;
+		bf->bf_mpdu = NULL;
 
 		/*
 		 * change the default rx antenna if rx diversity chooses the
@@ -584,9 +590,8 @@ int ath_rx_tasklet(struct ath_softc *sc, int flush)
 		} else {
 			sc->sc_rxotherant = 0;
 		}
-requeue:
-		list_move_tail(&bf->list, &sc->sc_rxbuf);
-		ath_rx_buf_link(sc, bf);
+rx_next:
+		ath_rx_requeue(sc, bf);
 	} while (1);
 
 	spin_unlock_bh(&sc->sc_rxbuflock);
