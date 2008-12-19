7c03701d88d50b5162da194060efa535e9a96bee
John W. Linville <linville@tuxdriver.com>
2008-12-19 13:59:31 -0500

    Revert "ath9k: Revamp transmit control block"
    This reverts commit f98d23d5efa95c8223ab293a0d43885c5b5eceb9.

--- drivers/net/wireless/ath9k/beacon.c
+++ drivers/net/wireless/ath9k/beacon.c
@@ -114,7 +114,7 @@ static void ath_beacon_setup(struct ath_softc *sc,
 	ath9k_hw_set11n_txdesc(ah, ds,
 			       skb->len + FCS_LEN,     /* frame length */
 			       ATH9K_PKT_TYPE_BEACON,  /* Atheros packet type */
-			       MAX_RATE_POWER,         /* FIXME */
+			       avp->av_btxctl.txpower, /* txpower XXX */
 			       ATH9K_TXKEYIX_INVALID,  /* no encryption */
 			       ATH9K_KEY_TYPE_CLEAR,   /* no encryption */
 			       flags                   /* no ack,
