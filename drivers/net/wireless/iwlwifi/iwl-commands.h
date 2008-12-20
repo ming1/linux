cf36ce460c2d0b3ee13be365640fcbd2509ba7c5
Daniel Wu <dyqith@gmail.com>
2008-12-20 10:53:29 -0800

    iwlwifi: Fix typo in iwl-commands.h for CCK rate bit range.
    My first (minor) patch, hopefully this is correct.
    
    Fix a typo in iwl-commands.h for CCK rates which needs 7 bits and not 4.
    
    Signed-off-by: Daniel Wu <dyqith@gmail.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/iwlwifi/iwl-commands.h
+++ drivers/net/wireless/iwlwifi/iwl-commands.h
@@ -294,7 +294,7 @@ struct iwl3945_power_per_rate {
  *        0x3)  54 Mbps
  *
  * Legacy CCK rate format for bits 7:0 (bit 8 must be "0", bit 9 "1"):
- *  3-0:   10)  1 Mbps
+ *  6-0:   10)  1 Mbps
  *         20)  2 Mbps
  *         55)  5.5 Mbps
  *        110)  11 Mbps
