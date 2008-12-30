bfc9e08f42c79cf6c2f909e9fbd9dcff9fa0fe70
Christian Lamparter <chunkeey@web.de>
2008-12-30 13:48:29 +0100

    p54: implement FIF_OTHER_BSS filter setting
    According to STMicroelectronics' LMAC documentation, the P54_FILTER_TYPE_TRANSPARENT flag
    "configures the receive frame filter to pass all frames without regard to type and address matching."
    
    Signed-off-by: Christian Lamparter <chunkeey@web.de>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/p54/p54common.c
+++ drivers/net/wireless/p54/p54common.c
@@ -1555,7 +1555,8 @@ static int p54_setup_mac(struct ieee80211_hw *dev)
 		 * "TRANSPARENT and PROMISCUOUS are mutually exclusive"
 		 * STSW45X0C LMAC API - page 12
 		 */
-		if ((priv->filter_flags & FIF_PROMISC_IN_BSS) &&
+		if (((priv->filter_flags & FIF_PROMISC_IN_BSS) ||
+		     (priv->filter_flags & FIF_OTHER_BSS)) &&
 		    (mode != P54_FILTER_TYPE_PROMISCUOUS))
 			mode |= P54_FILTER_TYPE_TRANSPARENT;
 	} else
@@ -1983,12 +1984,13 @@ static void p54_configure_filter(struct ieee80211_hw *dev,
 	struct p54_common *priv = dev->priv;
 
 	*total_flags &= FIF_PROMISC_IN_BSS |
+			FIF_OTHER_BSS |
 			(*total_flags & FIF_PROMISC_IN_BSS) ?
 				FIF_FCSFAIL : 0;
 
 	priv->filter_flags = *total_flags;
 
-	if (changed_flags & FIF_PROMISC_IN_BSS)
+	if (changed_flags & (FIF_PROMISC_IN_BSS | FIF_OTHER_BSS))
 		p54_setup_mac(dev);
 }
 
