3760bc878fc2e678b640d49c5e653a15b7a58f83
John W. Linville <linville@tuxdriver.com>
2009-01-05 17:07:15 -0500

    Revert "iwl3945: Use iwl-agn-rs.h rates definitions."
    This reverts commit 9eea6652252d5dfee20efcede5fb0ee529d7968d.

--- drivers/net/wireless/iwlwifi/iwl-3945.h
+++ drivers/net/wireless/iwlwifi/iwl-3945.h
@@ -233,7 +233,7 @@ struct iwl3945_clip_group {
 	const s8 clip_powers[IWL_MAX_RATES];
 };
 
-#include "iwl-agn-rs.h"
+#include "iwl-3945-rs.h"
 
 #define IWL_TX_FIFO_AC0	0
 #define IWL_TX_FIFO_AC1	1
