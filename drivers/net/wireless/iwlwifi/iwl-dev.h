706e6c33340f8f3b82a12ece1f04b1626a3c3bc2
John W. Linville <linville@tuxdriver.com>
2009-01-05 17:10:47 -0500

    Revert "iwlwifi: 5150 add support for 5150"
    This reverts commit df3c7972b14349c01cd3a2f0fab0c0a9c288610e.

--- drivers/net/wireless/iwlwifi/iwl-dev.h
+++ drivers/net/wireless/iwlwifi/iwl-dev.h
@@ -54,7 +54,6 @@ extern struct iwl_cfg iwl5100_agn_cfg;
 extern struct iwl_cfg iwl5350_agn_cfg;
 extern struct iwl_cfg iwl5100_bg_cfg;
 extern struct iwl_cfg iwl5100_abg_cfg;
-extern struct iwl_cfg iwl5150_agn_cfg;
 
 /* CT-KILL constants */
 #define CT_KILL_THRESHOLD	110 /* in Celsius */
