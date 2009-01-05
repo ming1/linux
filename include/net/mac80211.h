3c1828aa2294c5290520b111e915cae5f2e2ac54
John W. Linville <linville@tuxdriver.com>
2009-01-05 17:17:20 -0500

    Revert "mac80211: fix two kernel-doc warnings"
    This reverts commit 16c127f25dc26045908015cfc8a3a7dec26c2fb7.

--- include/net/mac80211.h
+++ include/net/mac80211.h
@@ -295,7 +295,7 @@ enum mac80211_rate_control_flags {
  *
  * @idx: rate index to attempt to send with
  * @flags: rate control flags (&enum mac80211_rate_control_flags)
- * @count: number of tries in this rate before going to the next rate
+ * @limit: number of retries before fallback
  *
  * A value of -1 for @idx indicates an invalid rate and, if used
  * in an array of retry rates, that no more rates should be tried.
@@ -1898,8 +1898,6 @@ struct ieee80211_sta *ieee80211_find_sta(struct ieee80211_hw *hw,
  * @short_preamble: whether mac80211 will request short-preamble transmission
  *	if the selected rate supports it
  * @max_rate_idx: user-requested maximum rate (not MCS for now)
- * @skb: the skb that will be transmitted, the control information in it needs
- *	to be filled in
  */
 struct ieee80211_tx_rate_control {
 	struct ieee80211_hw *hw;
