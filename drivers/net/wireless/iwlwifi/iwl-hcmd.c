35f064556e91cad887e4331a3a1c8c8d65adfc75
Zhu Yi <yi.zhu@intel.com>
2009-01-08 10:19:58 -0800

    iwlwifi: remove CMD_WANT_SKB flag if send_cmd_sync failure
    In function iwl_send_cmd_sync(), if the flag CMD_WANT_SKB is set but
    we are not provided with a valid SKB (cmd->meta.u.skb == NULL), we need
    to remove the CMD_WANT_SKB flag from the TX cmd queue. Otherwise in case
    the cmd comes in later, it will possibly set an invalid address. Thus
    it causes an invalid memory access.
    
    This fixed the bug http://bugzilla.kernel.org/show_bug.cgi?id=11326.
    
    Signed-off-by: Zhu Yi <yi.zhu@intel.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/iwlwifi/iwl-hcmd.c
+++ drivers/net/wireless/iwlwifi/iwl-hcmd.c
@@ -226,7 +226,7 @@ int iwl_send_cmd_sync(struct iwl_priv *priv, struct iwl_host_cmd *cmd)
 		IWL_ERR(priv, "Error: Response NULL in '%s'\n",
 			  get_cmd_string(cmd->id));
 		ret = -EIO;
-		goto out;
+		goto cancel;
 	}
 
 	ret = 0;
