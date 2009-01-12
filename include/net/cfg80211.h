7be2a9f40360c2fee582ca7090488bd291bd7d38
John W. Linville <linville@tuxdriver.com>
2009-01-12 16:12:27 -0500

    Revert "mac80211: Fix HT channel selection"
    This reverts commit 57674992816b2eba898438e27e380203d0f8d86f.

--- include/net/cfg80211.h
+++ include/net/cfg80211.h
@@ -563,7 +563,7 @@ struct cfg80211_ops {
 
 	int	(*set_channel)(struct wiphy *wiphy,
 			       struct ieee80211_channel *chan,
-			       enum nl80211_channel_type channel_type);
+			       enum nl80211_sec_chan_offset);
 };
 
 /* temporary wext handlers */
