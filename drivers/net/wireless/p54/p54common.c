7c04c26b404839c5a1b3a37587f40ca4a0c6e0c2
John W. Linville <linville@tuxdriver.com>
2009-01-19 12:01:36 -0500

    Revert "p54: set_tim must be atomic."
    This reverts commit 44b331cef5e083b127268020d3afcb455d9bb0d6.

--- drivers/net/wireless/p54/p54common.c
+++ drivers/net/wireless/p54/p54common.c
@@ -1295,7 +1295,7 @@ static int p54_set_tim(struct ieee80211_hw *dev, struct ieee80211_sta *sta,
 	struct p54_tim *tim;
 
 	skb = p54_alloc_skb(dev, P54_HDR_FLAG_CONTROL_OPSET, sizeof(*tim),
-			    P54_CONTROL_TYPE_TIM, GFP_ATOMIC);
+			    P54_CONTROL_TYPE_TIM, GFP_KERNEL);
 	if (!skb)
 		return -ENOMEM;
 
