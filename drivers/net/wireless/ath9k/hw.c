5a7c288bd712021d1b7369142d373875b4ad3f32
John W. Linville <linville@tuxdriver.com>
2009-03-16 21:28:22 -0400

    Revert "ath9k: AR9280 PCI devices must serialize IO as well"
    This reverts commit e98f9d8a5302241265ce8bfbb0b5baa4e43f9e27.

--- drivers/net/wireless/ath9k/hw.c
+++ drivers/net/wireless/ath9k/hw.c
@@ -628,15 +628,8 @@ static struct ath_hw *ath9k_hw_do_attach(u16 devid, struct ath_softc *sc,
 		goto bad;
 	}
 
-	/*
-	 * All PCI devices should be put here.
-	 * XXX: remove ah->is_pciexpress and use pdev->is_pcie, then
-	 * we can just check for !pdev->is_pcie here, but
-	 * consideration must be taken for handling AHB as well.
-	 */
 	if (ah->config.serialize_regmode == SER_REG_MODE_AUTO) {
-		if ((ah->hw_version.macVersion == AR_SREV_VERSION_5416_PCI) ||
-		     (AR_SREV_9280(ah) && !ah->is_pciexpress)) {
+		if (ah->hw_version.macVersion == AR_SREV_VERSION_5416_PCI) {
 			ah->config.serialize_regmode =
 				SER_REG_MODE_ON;
 		} else {
