71dde715d9df99422e9777312bad0e6e92a90b5e
John W. Linville <linville@tuxdriver.com>
2009-03-24 16:57:02 -0400

    Revert "iwl3945: release resources before shutting down"
    This reverts commit 1e6e3931ce090facedfe51cdcc318f6e9be95325.

--- drivers/net/wireless/iwlwifi/iwl3945-base.c
+++ drivers/net/wireless/iwlwifi/iwl3945-base.c
@@ -7718,12 +7718,7 @@ static void __devexit iwl3945_pci_remove(struct pci_dev *pdev)
 
 	set_bit(STATUS_EXIT_PENDING, &priv->status);
 
-	if (priv->mac80211_registered) {
-		ieee80211_unregister_hw(priv->hw);
-		priv->mac80211_registered = 0;
-	} else {
-		iwl3945_down(priv);
-	}
+	iwl3945_down(priv);
 
 	/* make sure we flush any pending irq or
 	 * tasklet for the driver
@@ -7745,6 +7740,9 @@ static void __devexit iwl3945_pci_remove(struct pci_dev *pdev)
 
 	iwl3945_unset_hw_params(priv);
 	iwl3945_clear_stations_table(priv);
+
+	if (priv->mac80211_registered)
+		ieee80211_unregister_hw(priv->hw);
 
 	/*netif_stop_queue(dev); */
 	flush_workqueue(priv->workqueue);
