b7556e09becc8cf51b33030447ca2ea4b27379c1
John W. Linville <linville@tuxdriver.com>
2009-03-24 16:52:50 -0400

    Revert "ath9k: Special processing for channel changes during scan"
    This reverts commit a554756fa6f0d4a27256bda2c0114631fdedddbd.

--- drivers/net/wireless/ath9k/ath9k.h
+++ drivers/net/wireless/ath9k/ath9k.h
@@ -634,7 +634,6 @@ struct ath_wiphy {
 		ATH_WIPHY_ACTIVE,
 		ATH_WIPHY_PAUSING,
 		ATH_WIPHY_PAUSED,
-		ATH_WIPHY_SCAN,
 	} state;
 	int chan_idx;
 	int chan_is_ht;
@@ -717,7 +716,6 @@ void ath9k_wiphy_chan_work(struct work_struct *work);
 bool ath9k_wiphy_started(struct ath_softc *sc);
 void ath9k_wiphy_pause_all_forced(struct ath_softc *sc,
 				  struct ath_wiphy *selected);
-bool ath9k_wiphy_scanning(struct ath_softc *sc);
 
 /*
  * Read and write, they both share the same lock. We do this to serialize
