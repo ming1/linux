aa2d280b6931284da7c932adfdcc3bce25243386
John W. Linville <linville@tuxdriver.com>
2009-03-24 16:53:20 -0400

    Revert "cfg80211: pass the regulatory_request to ignore_request"
    This reverts commit e23234bcbbc9d8339732f12e24d5a6aedd98abc3.

--- net/wireless/reg.c
+++ net/wireless/reg.c
@@ -1232,8 +1232,8 @@ static int reg_copy_regd(const struct ieee80211_regdomain **dst_regd,
 
 /* This has the logic which determines when a new request
  * should be ignored. */
-static int ignore_request(struct wiphy *wiphy,
-			  struct regulatory_request *pending_request)
+static int ignore_request(struct wiphy *wiphy, enum reg_set_by set_by,
+			  const char *alpha2)
 {
 	struct wiphy *last_wiphy = NULL;
 
@@ -1243,7 +1243,7 @@ static int ignore_request(struct wiphy *wiphy,
 	if (!last_request)
 		return 0;
 
-	switch (pending_request->initiator) {
+	switch (set_by) {
 	case REGDOM_SET_BY_INIT:
 		return -EINVAL;
 	case REGDOM_SET_BY_CORE:
@@ -1252,7 +1252,7 @@ static int ignore_request(struct wiphy *wiphy,
 
 		last_wiphy = wiphy_idx_to_wiphy(last_request->wiphy_idx);
 
-		if (unlikely(!is_an_alpha2(pending_request->alpha2)))
+		if (unlikely(!is_an_alpha2(alpha2)))
 			return -EINVAL;
 		if (last_request->initiator == REGDOM_SET_BY_COUNTRY_IE) {
 			if (last_wiphy != wiphy) {
@@ -1262,7 +1262,7 @@ static int ignore_request(struct wiphy *wiphy,
 				 * intersect them, but that seems unlikely
 				 * to be correct. Reject second one for now.
 				 */
-				if (regdom_changes(pending_request->alpha2))
+				if (regdom_changes(alpha2))
 					return -EOPNOTSUPP;
 				return -EALREADY;
 			}
@@ -1270,7 +1270,7 @@ static int ignore_request(struct wiphy *wiphy,
 			 * Two consecutive Country IE hints on the same wiphy.
 			 * This should be picked up early by the driver/stack
 			 */
-			if (WARN_ON(regdom_changes(pending_request->alpha2)))
+			if (WARN_ON(regdom_changes(alpha2)))
 				return 0;
 			return -EALREADY;
 		}
@@ -1279,7 +1279,7 @@ static int ignore_request(struct wiphy *wiphy,
 		if (last_request->initiator == REGDOM_SET_BY_CORE) {
 			if (is_old_static_regdom(cfg80211_regdomain))
 				return 0;
-			if (regdom_changes(pending_request->alpha2))
+			if (regdom_changes(alpha2))
 				return 0;
 			return -EALREADY;
 		}
@@ -1290,7 +1290,7 @@ static int ignore_request(struct wiphy *wiphy,
 		 * loaded card also agrees on the regulatory domain.
 		 */
 		if (last_request->initiator == REGDOM_SET_BY_DRIVER &&
-		    !regdom_changes(pending_request->alpha2))
+		    !regdom_changes(alpha2))
 			return -EALREADY;
 
 		return REG_INTERSECT;
@@ -1316,7 +1316,7 @@ static int ignore_request(struct wiphy *wiphy,
 		}
 
 		if (!is_old_static_regdom(cfg80211_regdomain) &&
-		    !regdom_changes(pending_request->alpha2))
+		    !regdom_changes(alpha2))
 			return -EALREADY;
 
 		return 0;
@@ -1347,7 +1347,9 @@ static int __regulatory_hint(struct wiphy *wiphy,
 
 	assert_cfg80211_lock();
 
-	r = ignore_request(wiphy, pending_request);
+	r = ignore_request(wiphy,
+			  pending_request->initiator,
+			  pending_request->alpha2);
 
 	if (r == REG_INTERSECT) {
 		if (pending_request->initiator == REGDOM_SET_BY_DRIVER) {
