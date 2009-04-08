19ec899f5411c0d6596edb2e85cf2416f4cacd2e
John W. Linville <linville@tuxdriver.com>
2009-04-08 14:54:11 -0400

    Revert "ath9k: remove write-only current_rd_inuse"
    This reverts commit 17d8ab94a86e08997fd03dfc26c93fc3ccc83b2e.

--- drivers/net/wireless/ath9k/regd.c
+++ drivers/net/wireless/ath9k/regd.c
@@ -433,6 +433,7 @@ int ath9k_regd_init(struct ath_hw *ah)
 			regdmn = country->regDmnEnum;
 	}
 
+	ah->regulatory.current_rd_inuse = regdmn;
 	ah->regulatory.regpair = ath9k_get_regpair(regdmn);
 
 	if (!ah->regulatory.regpair) {
