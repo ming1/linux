a8fbcab96f7784645f69e5432b5a659625f4e3e5
John W. Linville <linville@tuxdriver.com>
2009-04-08 14:55:05 -0400

    Revert "orinoco: Rename orinoco.c"
    This reverts commit 96aa16fe3a1512b0aa629b3ae280ba0bb16f1ada.

--- drivers/net/wireless/orinoco/orinoco_cs.c
+++ drivers/net/wireless/orinoco/orinoco_cs.c
@@ -7,7 +7,7 @@
  * Linksys, D-Link and Farallon Skyline. It should also work on Symbol
  * cards such as the 3Com AirConnect and Ericsson WLAN.
  *
- * Copyright notice & release notes in file main.c
+ * Copyright notice & release notes in file orinoco.c
  */
 
 #define DRIVER_NAME "orinoco_cs"
