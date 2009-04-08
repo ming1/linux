b55bffc751dde56274c44277a49c91deea792426
John W. Linville <linville@tuxdriver.com>
2009-04-08 14:54:27 -0400

    Revert "cfg80211: free_priv for BSS info"
    This reverts commit 406a5f25154dbf586f20c1feb22f2e4d7c142ffa.

--- net/wireless/scan.c
+++ net/wireless/scan.c
@@ -56,8 +56,6 @@ static void bss_release(struct kref *ref)
 	struct cfg80211_internal_bss *bss;
 
 	bss = container_of(ref, struct cfg80211_internal_bss, ref);
-	if (bss->pub.free_priv)
-		bss->pub.free_priv(&bss->pub);
 	kfree(bss);
 }
 
