87dda15125385e22a7e3c0c6d88806d0d488dae0
John W. Linville <linville@tuxdriver.com>
2009-04-30 10:47:25 -0400

    Revert "atheros: introduce ath module containing common ath5k/ath9k/ar9170 code"
    This reverts commit 1ad8fcc03a9ff20697c5f59c1458912f3e3c2a64.

--- drivers/net/wireless/ath9k/hw.h
+++ drivers/net/wireless/ath9k/hw.h
@@ -25,10 +25,9 @@
 #include "ani.h"
 #include "eeprom.h"
 #include "calib.h"
+#include "regd.h"
 #include "reg.h"
 #include "phy.h"
-
-#include "../ath/regd.h"
 
 #define ATHEROS_VENDOR_ID	0x168c
 #define AR5416_DEVID_PCI	0x0023
@@ -405,7 +404,7 @@ struct ath_hw {
 	struct ath9k_hw_version hw_version;
 	struct ath9k_ops_config config;
 	struct ath9k_hw_capabilities caps;
-	struct ath_regulatory regulatory;
+	struct ath9k_regulatory regulatory;
 	struct ath9k_channel channels[38];
 	struct ath9k_channel *curchan;
 
