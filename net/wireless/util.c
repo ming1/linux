78e0b3882fba810de8c3169d0727408ff39de1f7
John W. Linville <linville@tuxdriver.com>
2009-06-03 11:16:42 -0400

    Revert "cfg80211: use key size constants"
    This reverts commit 304cc91ea7447b425607d77ad5aefbf54a1bc260.

--- net/wireless/util.c
+++ net/wireless/util.c
@@ -157,25 +157,26 @@ int cfg80211_validate_key_settings(struct key_params *params, int key_idx,
 	    params->cipher != WLAN_CIPHER_SUITE_WEP104)
 		return -EINVAL;
 
+	/* TODO: add definitions for the lengths to linux/ieee80211.h */
 	switch (params->cipher) {
 	case WLAN_CIPHER_SUITE_WEP40:
-		if (params->key_len != WLAN_KEY_LEN_WEP40)
+		if (params->key_len != 5)
 			return -EINVAL;
 		break;
 	case WLAN_CIPHER_SUITE_TKIP:
-		if (params->key_len != WLAN_KEY_LEN_TKIP)
+		if (params->key_len != 32)
 			return -EINVAL;
 		break;
 	case WLAN_CIPHER_SUITE_CCMP:
-		if (params->key_len != WLAN_KEY_LEN_CCMP)
+		if (params->key_len != 16)
 			return -EINVAL;
 		break;
 	case WLAN_CIPHER_SUITE_WEP104:
-		if (params->key_len != WLAN_KEY_LEN_WEP104)
+		if (params->key_len != 13)
 			return -EINVAL;
 		break;
 	case WLAN_CIPHER_SUITE_AES_CMAC:
-		if (params->key_len != WLAN_KEY_LEN_AES_CMAC)
+		if (params->key_len != 16)
 			return -EINVAL;
 		break;
 	default:
