cea61f6a608332ca12007053c2b9e7039c754262
John W. Linville <linville@tuxdriver.com>
2009-06-10 13:55:01 -0400

    Revert "mac80211: disable powersave if pm_qos asks for low latency"
    This reverts commit dfb02ae407241573881c064d77ff738a75a39674.

--- net/mac80211/mlme.c
+++ net/mac80211/mlme.c
@@ -17,7 +17,6 @@
 #include <linux/if_arp.h>
 #include <linux/etherdevice.h>
 #include <linux/rtnetlink.h>
-#include <linux/pm_qos_params.h>
 #include <net/mac80211.h>
 #include <asm/unaligned.h>
 
@@ -516,7 +515,7 @@ static void ieee80211_change_ps(struct ieee80211_local *local)
 }
 
 /* need to hold RTNL or interface lock */
-void ieee80211_recalc_ps(struct ieee80211_local *local, s32 latency)
+void ieee80211_recalc_ps(struct ieee80211_local *local)
 {
 	struct ieee80211_sub_if_data *sdata, *found = NULL;
 	int count = 0;
@@ -535,22 +534,10 @@ void ieee80211_recalc_ps(struct ieee80211_local *local, s32 latency)
 		count++;
 	}
 
-	if (count == 1 && found->u.mgd.powersave) {
-		s32 beaconint_us;
-
-		if (latency < 0)
-			latency = pm_qos_requirement(PM_QOS_NETWORK_LATENCY);
-
-		beaconint_us = ieee80211_tu_to_usec(
-					found->vif.bss_conf.beacon_int);
-
-		if (beaconint_us > latency)
-			local->ps_sdata = NULL;
-		else
-			local->ps_sdata = found;
-	} else {
+	if (count == 1 && found->u.mgd.powersave)
+		local->ps_sdata = found;
+	else
 		local->ps_sdata = NULL;
-	}
 
 	ieee80211_change_ps(local);
 }
@@ -2336,19 +2323,4 @@ void ieee80211_mlme_notify_scan_completed(struct ieee80211_local *local)
 	list_for_each_entry_rcu(sdata, &local->interfaces, list)
 		ieee80211_restart_sta_timer(sdata);
 	rcu_read_unlock();
-}
-
-int ieee80211_max_network_latency(struct notifier_block *nb,
-				  unsigned long data, void *dummy)
-{
-	s32 latency_usec = (s32) data;
-	struct ieee80211_local *local =
-		container_of(nb, struct ieee80211_local,
-			     network_latency_notifier);
-
-	mutex_lock(&local->iflist_mtx);
-	ieee80211_recalc_ps(local, latency_usec);
-	mutex_unlock(&local->iflist_mtx);
-
-	return 0;
 }
