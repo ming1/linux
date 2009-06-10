3ebeed7640fd4466e0d99ecb46ee34787bef5ee6
John W. Linville <linville@tuxdriver.com>
2009-06-10 13:55:26 -0400

    Revert "iwl3945: use iwl_mac_remove_interface from iwlwifi"
    This reverts commit 78913adfcc0710a3b61f9a1c6b4cc9cc2bd751b0.

--- drivers/net/wireless/iwlwifi/iwl-core.h
+++ drivers/net/wireless/iwlwifi/iwl-core.h
@@ -276,8 +276,6 @@ int iwl_commit_rxon(struct iwl_priv *priv);
 int iwl_set_mode(struct iwl_priv *priv, int mode);
 int iwl_mac_add_interface(struct ieee80211_hw *hw,
 				 struct ieee80211_if_init_conf *conf);
-void iwl_mac_remove_interface(struct ieee80211_hw *hw,
-				 struct ieee80211_if_init_conf *conf);
 
 /*****************************************************
  * RX handlers.
