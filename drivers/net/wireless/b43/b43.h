6ee2eaefbf7d24a79ce7c7d003fb2192f2cafd3e
John W. Linville <linville@tuxdriver.com>
2009-06-30 16:21:19 -0400

    Revert "b43: Remove unnecessary MMIO in interrupt hotpath"
    This reverts commit e352b93739f7532bb9cea7e720d468723b9521c0.

--- drivers/net/wireless/b43/b43.h
+++ drivers/net/wireless/b43/b43.h
@@ -778,8 +778,8 @@ struct b43_wldev {
 	/* Reason code of the last interrupt. */
 	u32 irq_reason;
 	u32 dma_reason[6];
-	/* The currently active generic-interrupt mask. */
-	u32 irq_mask;
+	/* saved irq enable/disable state bitfield. */
+	u32 irq_savedstate;
 	/* Link Quality calculation context. */
 	struct b43_noise_calculation noisecalc;
 	/* if > 0 MAC is suspended. if == 0 MAC is enabled. */
