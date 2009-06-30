d4dd3dd8931055f4f9f0e4170671bc2f8f05b611
John W. Linville <linville@tuxdriver.com>
2009-06-30 16:16:04 -0400

    Revert "iwlcore: support ICT interrupt"
    This reverts commit 30b2195c3d334895b7340e91a4cfe228a0c8d9fd.

--- drivers/net/wireless/iwlwifi/iwl-5000.c
+++ drivers/net/wireless/iwlwifi/iwl-5000.c
@@ -1487,7 +1487,6 @@ struct iwl_lib_ops iwl5000_lib = {
 		.query_addr = iwl5000_eeprom_query_addr,
 	},
 	.post_associate = iwl_post_associate,
-	.isr = iwl_isr_ict,
 	.config_ap = iwl_config_ap,
 	.temp_ops = {
 		.temperature = iwl5000_temperature,
@@ -1537,7 +1536,6 @@ static struct iwl_lib_ops iwl5150_lib = {
 		.query_addr = iwl5000_eeprom_query_addr,
 	},
 	.post_associate = iwl_post_associate,
-	.isr = iwl_isr_ict,
 	.config_ap = iwl_config_ap,
 	.temp_ops = {
 		.temperature = iwl5150_temperature,
