a05418d9e21722d0157ec26efcfad1a0d1fc441c
John W. Linville <linville@tuxdriver.com>
2009-07-24 11:46:32 -0400

    Revert "iwlwifi: only show active power level via sysfs"
    This reverts commit 864d705348c36b5dee7ba9d80a678bfa916f5662.

--- drivers/net/wireless/iwlwifi/iwl3945-base.c
+++ drivers/net/wireless/iwlwifi/iwl3945-base.c
@@ -3573,10 +3573,12 @@ static ssize_t show_power_level(struct device *d,
 				struct device_attribute *attr, char *buf)
 {
 	struct iwl_priv *priv = dev_get_drvdata(d);
+	int mode = priv->power_data.user_power_setting;
 	int level = priv->power_data.power_mode;
 	char *p = buf;
 
-	p += sprintf(p, "%d\n", level);
+	p += sprintf(p, "INDEX:%d\t", level);
+	p += sprintf(p, "USER:%d\n", mode);
 	return p - buf + 1;
 }
 
