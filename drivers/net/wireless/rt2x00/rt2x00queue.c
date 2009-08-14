fda2e1d0db9a6453cc7a1c34718b58bd4e66522a
John W. Linville <linville@tuxdriver.com>
2009-08-14 09:56:13 -0400

    Revert "rt2x00: Use IEEE80211_TX_CTL_MORE_FRAMES flag"
    This reverts commit d31a80f10f30e352eaffec8011672b2a74c04d57.

--- drivers/net/wireless/rt2x00/rt2x00queue.c
+++ drivers/net/wireless/rt2x00/rt2x00queue.c
@@ -324,8 +324,7 @@ static void rt2x00queue_create_tx_descriptor(struct queue_entry *entry,
 	/*
 	 * Check if more fragments are pending
 	 */
-	if (ieee80211_has_morefrags(hdr->frame_control) ||
-	    (tx_info->flags & IEEE80211_TX_CTL_MORE_FRAMES)) {
+	if (ieee80211_has_morefrags(hdr->frame_control)) {
 		__set_bit(ENTRY_TXD_BURST, &txdesc->flags);
 		__set_bit(ENTRY_TXD_MORE_FRAG, &txdesc->flags);
 	}
