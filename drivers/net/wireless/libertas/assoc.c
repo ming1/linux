8a6328f23f50f796dc8eb5ca077194599f6a2441
Andrey Yurovsky <andrey@cozybit.com>
2009-08-13 17:34:40 -0700

    libertas: don't use dynamic-sized array
    sparse complains about a bad constant expression due to the use of a
    dynamic-sized array in get_common_rates().  Allocate and free the array
    instead.
    
    Signed-off-by: Andrey Yurovsky <andrey@cozybit.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/libertas/assoc.c
+++ drivers/net/wireless/libertas/assoc.c
@@ -45,8 +45,12 @@ static int get_common_rates(struct lbs_private *priv,
 {
 	u8 *card_rates = lbs_bg_rates;
 	int i, j;
-	u8 tmp[MAX_RATES * ARRAY_SIZE(lbs_bg_rates)];
+	u8 *tmp;
 	size_t tmp_size = 0;
+
+	tmp = kzalloc(MAX_RATES * ARRAY_SIZE(lbs_bg_rates), GFP_KERNEL);
+	if (!tmp)
+		return -1;
 
 	/* For each rate in card_rates that exists in rate1, copy to tmp */
 	for (i = 0; i < ARRAY_SIZE(lbs_bg_rates) && card_rates[i]; i++) {
@@ -78,6 +82,7 @@ static int get_common_rates(struct lbs_private *priv,
 			return -1;
 		}
 	}
+	kfree(tmp);
 	return 0;
 }
 
