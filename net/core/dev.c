4f1ca57d24459791dce5dd6e1354c9ce29200fd7
John W. Linville <linville@tuxdriver.com>
2009-08-24 14:23:11 -0400

    Revert "net: export __dev_addr_sync/__dev_addr_unsync"
    This reverts commit 4d7ec76aa9fe095fbce1241b42434a360a17b7b6.

--- net/core/dev.c
+++ net/core/dev.c
@@ -3927,7 +3927,6 @@ int __dev_addr_sync(struct dev_addr_list **to, int *to_count,
 	}
 	return err;
 }
-EXPORT_SYMBOL_GPL(__dev_addr_sync);
 
 void __dev_addr_unsync(struct dev_addr_list **to, int *to_count,
 		       struct dev_addr_list **from, int *from_count)
@@ -3947,7 +3946,6 @@ void __dev_addr_unsync(struct dev_addr_list **to, int *to_count,
 		da = next;
 	}
 }
-EXPORT_SYMBOL_GPL(__dev_addr_unsync);
 
 /**
  *	dev_unicast_sync - Synchronize device's unicast list to another device
