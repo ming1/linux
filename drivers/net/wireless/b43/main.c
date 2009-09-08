9de2d7b7962c32c4e8f66b184c97d130d56e4b62
John W. Linville <linville@tuxdriver.com>
2009-09-08 14:34:09 -0400

    Revert "b43: Implement antenna diversity support for LP-PHY"
    This reverts commit 30e0f22192ac55f18c464f61e08b2075c4a0e942.

--- drivers/net/wireless/b43/main.c
+++ drivers/net/wireless/b43/main.c
@@ -1456,8 +1456,7 @@ static u16 b43_antenna_to_phyctl(int antenna)
 		return B43_TXH_PHY_ANT2;
 	case B43_ANTENNA3:
 		return B43_TXH_PHY_ANT3;
-	case B43_ANTENNA_AUTO0:
-	case B43_ANTENNA_AUTO1:
+	case B43_ANTENNA_AUTO:
 		return B43_TXH_PHY_ANT01AUTO;
 	}
 	B43_WARN_ON(1);
