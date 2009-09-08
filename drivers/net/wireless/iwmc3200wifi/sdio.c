7b1609af71b9b60e1dad6a5cdc51403afcf1a8c4
John W. Linville <linville@tuxdriver.com>
2009-09-08 14:44:32 -0400

    Revert "imwc3200: move iwmc3200 SDIO ids to sdio_ids.h"
    This reverts commit ca10c022a2afc21b636a1170aad2134ae5e8a39f.

--- drivers/net/wireless/iwmc3200wifi/sdio.c
+++ drivers/net/wireless/iwmc3200wifi/sdio.c
@@ -65,7 +65,6 @@
 #include <linux/kernel.h>
 #include <linux/netdevice.h>
 #include <linux/debugfs.h>
-#include <linux/mmc/sdio_ids.h>
 #include <linux/mmc/sdio.h>
 #include <linux/mmc/sdio_func.h>
 
@@ -493,8 +492,7 @@ static void iwm_sdio_remove(struct sdio_func *func)
 }
 
 static const struct sdio_device_id iwm_sdio_ids[] = {
-	{ SDIO_DEVICE(SDIO_VENDOR_ID_INTEL,
-		      SDIO_DEVICE_ID_INTEL_IWMC3200WIFI) },
+	{ SDIO_DEVICE(SDIO_VENDOR_ID_INTEL, SDIO_DEVICE_ID_IWM) },
 	{ /* end: all zeroes */	},
 };
 MODULE_DEVICE_TABLE(sdio, iwm_sdio_ids);
