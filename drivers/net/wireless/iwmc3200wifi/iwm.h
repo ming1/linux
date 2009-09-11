1ba0fc45f75d67060f30ef679b6fa4cac5ad6f8b
John W. Linville <linville@tuxdriver.com>
2009-09-11 11:25:55 -0400

    Revert "iwmc3200wifi: simplify calibration map"
    This reverts commit ab5adc1b9f76af0585399b28a9ef209df18777d9.

--- drivers/net/wireless/iwmc3200wifi/iwm.h
+++ drivers/net/wireless/iwmc3200wifi/iwm.h
@@ -65,7 +65,8 @@
 
 struct iwm_conf {
 	u32 sdio_ior_timeout;
-	unsigned long calib_map;
+	unsigned long init_calib_map;
+	unsigned long periodic_calib_map;
 	bool reset_on_fatal_err;
 	bool auto_connect;
 	bool wimax_not_present;
