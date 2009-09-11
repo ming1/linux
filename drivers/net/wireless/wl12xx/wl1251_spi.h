fde61abefa8eb2f65e8844985a041bc69e50e0e4
John W. Linville <linville@tuxdriver.com>
2009-09-11 11:22:28 -0400

    Revert "wl1251: rename reg.h to wl1251_reg.h"
    This reverts commit c9e4e10c0a68c98b81c05ba1b179a528c7a55053.

--- drivers/net/wireless/wl12xx/wl1251_spi.h
+++ drivers/net/wireless/wl12xx/wl1251_spi.h
@@ -27,7 +27,7 @@
 
 #include "wl1251_cmd.h"
 #include "wl1251_acx.h"
-#include "wl1251_reg.h"
+#include "reg.h"
 
 #define WSPI_CMD_READ                 0x40000000
 #define WSPI_CMD_WRITE                0x00000000
