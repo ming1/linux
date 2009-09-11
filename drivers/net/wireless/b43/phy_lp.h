739a80f82e7cec03de6f38425c3e66fc21f7aa19
John W. Linville <linville@tuxdriver.com>
2009-09-11 11:21:38 -0400

    Revert "b43: LP-PHY: Initialize TX power control"
    This reverts commit 1badbc4f9ad85f265d831d0b886d506b18d86bbe.

--- drivers/net/wireless/b43/phy_lp.h
+++ drivers/net/wireless/b43/phy_lp.h
@@ -886,11 +886,6 @@ struct b43_phy_lp {
 	u16 dig_flt_state[9];
 };
 
-enum tssi_mux_mode {
-	TSSI_MUX_PREPA,
-	TSSI_MUX_POSTPA,
-	TSSI_MUX_EXT,
-};
 
 struct b43_phy_operations;
 extern const struct b43_phy_operations b43_phyops_lp;
