914752517befd375e6c9e4c657916eec551fd53b
John W. Linville <linville@tuxdriver.com>
2009-09-11 11:19:20 -0400

    Revert "ath9k: Enable btcoex based on the subsystem id of the device"
    This reverts commit 21dacb00fde4e9fd7fc95d0f32ed0bb43ee2d72f.

--- drivers/net/wireless/ath/ath9k/hw.c
+++ drivers/net/wireless/ath/ath9k/hw.c
@@ -3690,8 +3690,7 @@ void ath9k_hw_fill_cap_info(struct ath_hw *ah)
 	pCap->num_antcfg_2ghz =
 		ah->eep_ops->get_num_ant_config(ah, ATH9K_HAL_FREQ_BAND_2GHZ);
 
-	if (AR_SREV_9280_10_OR_LATER(ah) &&
-	    ath_btcoex_supported(ah->hw_version.subsysid)) {
+	if (AR_SREV_9280_10_OR_LATER(ah) && btcoex_enable) {
 		btcoex_info->btactive_gpio = ATH_BTACTIVE_GPIO;
 		btcoex_info->wlanactive_gpio = ATH_WLANACTIVE_GPIO;
 
