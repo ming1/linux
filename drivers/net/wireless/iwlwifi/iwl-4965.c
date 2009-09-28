54e700e189f30c12727518c0fa2ba127e067f391
John W. Linville <linville@tuxdriver.com>
2009-09-28 14:09:31 -0400

    Revert "iwlwifi: disable powersave for 4965"
    This reverts commit b014709df07c3fbced0d32c95f503d81c7a1755e.

--- drivers/net/wireless/iwlwifi/iwl-4965.c
+++ drivers/net/wireless/iwlwifi/iwl-4965.c
@@ -2346,7 +2346,6 @@ struct iwl_cfg iwl4965_agn_cfg = {
 	.mod_params = &iwl4965_mod_params,
 	.use_isr_legacy = true,
 	.ht_greenfield_support = false,
-	.broken_powersave = true,
 };
 
 /* Module firmware */
