0de0b9dae02dcd3716f88ac86405e953a44db0ca
John W. Linville <linville@tuxdriver.com>
2009-09-28 14:19:38 -0400

    Revert "ath9k: call ath9k_hw_detach() once upon hw init failure"
    This reverts commit 61ff987d0f3b69f3d59a5de8ff013fae41997e66.

--- drivers/net/wireless/ath/ath9k/main.c
+++ drivers/net/wireless/ath/ath9k/main.c
@@ -1520,7 +1520,8 @@ bad2:
 		if (ATH_TXQ_SETUP(sc, i))
 			ath_tx_cleanupq(sc, &sc->tx.txq[i]);
 bad:
-	ath9k_hw_detach(ah);
+	if (ah)
+		ath9k_hw_detach(ah);
 	sc->sc_ah = NULL;
 bad_no_ah:
 	ath9k_exit_debug(sc);
