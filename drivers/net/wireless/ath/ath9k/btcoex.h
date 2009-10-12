52137b238bb0650bbd82c6aa9c0b0cc7768ac1ee
John W. Linville <linville@tuxdriver.com>
2009-10-12 14:14:02 -0400

    Revert "ath9k: rename btcoex_scheme to just scheme"
    This reverts commit ceaccab57e619a6221c2404ce496cc5f74cc3d02.

--- drivers/net/wireless/ath/ath9k/btcoex.h
+++ drivers/net/wireless/ath/ath9k/btcoex.h
@@ -56,7 +56,7 @@ struct ath_btcoex_config {
 };
 
 struct ath_btcoex_info {
-	enum ath_btcoex_scheme scheme;
+	enum ath_btcoex_scheme btcoex_scheme;
 	bool enabled;
 	u8 wlanactive_gpio;
 	u8 btactive_gpio;
