8c9894645b86c97dd500ea21e8b7d8209cb151b0
John W. Linville <linville@tuxdriver.com>
2009-11-20 11:46:13 -0500

    Revert "wl1271: add workaround to avoid distortion due to excessive tx power"
    This reverts commit 83d4d17db6a6e233f1b69c758817748f7479e65f.

--- drivers/net/wireless/wl12xx/wl1271_acx.c
+++ drivers/net/wireless/wl12xx/wl1271_acx.c
@@ -137,12 +137,7 @@ int wl1271_acx_tx_power(struct wl1271 *wl, int power)
 		goto out;
 	}
 
-	/*
-	 * FIXME: This is a workaround needed while we don't the correct
-	 * calibration, to avoid distortions
-	 */
-	/* acx->current_tx_power = power * 10; */
-	acx->current_tx_power = 70;
+	acx->current_tx_power = power * 10;
 
 	ret = wl1271_cmd_configure(wl, DOT11_CUR_TX_PWR, acx, sizeof(*acx));
 	if (ret < 0) {
