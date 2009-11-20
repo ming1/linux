63abfcf2dc5535313c16a09d78e490d487e67580
John W. Linville <linville@tuxdriver.com>
2009-11-20 11:31:35 -0500

    Revert "rfkill: Add constant for RFKILL_TYPE_FM radio devices"
    This reverts commit e1c31ea1444498a30036419cbfd754084391ba74.

--- net/rfkill/core.c
+++ net/rfkill/core.c
@@ -592,13 +592,11 @@ static const char *rfkill_get_type_str(enum rfkill_type type)
 		return "wwan";
 	case RFKILL_TYPE_GPS:
 		return "gps";
-	case RFKILL_TYPE_FM:
-		return "fm";
 	default:
 		BUG();
 	}
 
-	BUILD_BUG_ON(NUM_RFKILL_TYPES != RFKILL_TYPE_FM + 1);
+	BUILD_BUG_ON(NUM_RFKILL_TYPES != RFKILL_TYPE_GPS + 1);
 }
 
 static ssize_t rfkill_type_show(struct device *dev,
