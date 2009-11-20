03e41b49549fac0ff1e48767b40f44a93f83e3cb
John W. Linville <linville@tuxdriver.com>
2009-11-20 11:34:18 -0500

    Revert "mac80211: update PERR frame format"
    This reverts commit 9e0d1b99f0b6b1cde7f6ebbe673e07d12893281c.

--- net/mac80211/mesh_pathtbl.c
+++ net/mac80211/mesh_pathtbl.c
@@ -465,9 +465,8 @@ void mesh_plink_broken(struct sta_info *sta)
 			mpath->flags &= ~MESH_PATH_ACTIVE;
 			++mpath->dsn;
 			spin_unlock_bh(&mpath->state_lock);
-			mesh_path_error_tx(MESH_TTL, mpath->dst,
+			mesh_path_error_tx(mpath->dst,
 					cpu_to_le32(mpath->dsn),
-					PERR_RCODE_DEST_UNREACH,
 					sdata->dev->broadcast, sdata);
 		} else
 		spin_unlock_bh(&mpath->state_lock);
@@ -612,8 +611,7 @@ void mesh_path_discard_frame(struct sk_buff *skb,
 		mpath = mesh_path_lookup(da, sdata);
 		if (mpath)
 			dsn = ++mpath->dsn;
-		mesh_path_error_tx(MESH_TTL, skb->data, cpu_to_le32(dsn),
-				   PERR_RCODE_NO_ROUTE, ra, sdata);
+		mesh_path_error_tx(skb->data, cpu_to_le32(dsn), ra, sdata);
 	}
 
 	kfree_skb(skb);
