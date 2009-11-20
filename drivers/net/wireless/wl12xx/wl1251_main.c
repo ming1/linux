e8e8781d8650b716fb2b94cb1be7901257f7c68b
John W. Linville <linville@tuxdriver.com>
2009-11-20 11:32:05 -0500

    Revert "wl1251: mask aid bits 14 and 15 in ps-poll template"
    This reverts commit b744d548c8677176f821cef55a77ebc1608dc555.

--- drivers/net/wireless/wl12xx/wl1251_main.c
+++ drivers/net/wireless/wl12xx/wl1251_main.c
@@ -580,10 +580,7 @@ static int wl1251_build_ps_poll(struct wl1251 *wl, u16 aid)
 
 	memcpy(template.bssid, wl->bssid, ETH_ALEN);
 	memcpy(template.ta, wl->mac_addr, ETH_ALEN);
-
-	/* aid in PS-Poll has its two MSBs each set to 1 */
-	template.aid = cpu_to_le16(1 << 15 | 1 << 14 | aid);
-
+	template.aid = aid;
 	template.fc = cpu_to_le16(IEEE80211_FTYPE_CTL | IEEE80211_STYPE_PSPOLL);
 
 	return wl1251_cmd_template_set(wl, CMD_PS_POLL, &template,
