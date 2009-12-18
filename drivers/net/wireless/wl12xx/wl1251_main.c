5cbb57ea2ff660a2c9549216958322cb5b189fcb
John W. Linville <linville@tuxdriver.com>
2009-12-18 15:19:38 -0500

    Revert "wl1251: Send null data packet with "TODS" bit set"
    This reverts commit 5b4fa13de63255408cdee27e7c7980c2f461f972.

--- drivers/net/wireless/wl12xx/wl1251_main.c
+++ drivers/net/wireless/wl12xx/wl1251_main.c
@@ -576,8 +576,7 @@ static int wl1251_build_null_data(struct wl1251 *wl)
 
 	memcpy(template.header.sa, wl->mac_addr, ETH_ALEN);
 	template.header.frame_ctl = cpu_to_le16(IEEE80211_FTYPE_DATA |
-						IEEE80211_STYPE_NULLFUNC |
-						IEEE80211_FCTL_TODS);
+						IEEE80211_STYPE_NULLFUNC);
 
 	return wl1251_cmd_template_set(wl, CMD_NULL_DATA, &template,
 				       sizeof(template));
