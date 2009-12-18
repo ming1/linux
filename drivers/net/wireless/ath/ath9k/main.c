dc94735d3d73d4679d39dfd7c1ee4505b344227b
John W. Linville <linville@tuxdriver.com>
2009-12-18 15:37:03 -0500

    Revert "ath9k: Define bus agnostic bluetooth coex prep helper"
    This reverts commit 23112e328bd13fc057e0ae34591f560c2487b102.

--- drivers/net/wireless/ath/ath9k/main.c
+++ drivers/net/wireless/ath/ath9k/main.c
@@ -2243,8 +2243,7 @@ static int ath9k_start(struct ieee80211_hw *hw)
 					   AR_STOMP_LOW_WLAN_WGHT);
 		ath9k_hw_btcoex_enable(ah);
 
-		if (sc->bus_ops->bt_coex_prep)
-			sc->bus_ops->bt_coex_prep(sc);
+		ath_pcie_aspm_disable(sc);
 		if (ah->btcoex_hw.scheme == ATH_BTCOEX_CFG_3WIRE)
 			ath9k_btcoex_timer_resume(sc);
 	}
