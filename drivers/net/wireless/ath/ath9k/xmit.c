e7c91a6f0b1aecc1c6be0aad46720c30000d7cbe
John W. Linville <linville@tuxdriver.com>
2009-12-21 11:17:40 -0500

    Revert "ath9k: Fix TX queue draining"
    This reverts commit de606d9e3b8d7f6be97c613f878fed9f05426959.

--- drivers/net/wireless/ath/ath9k/xmit.c
+++ drivers/net/wireless/ath/ath9k/xmit.c
@@ -1108,11 +1108,11 @@ void ath_drain_all_txq(struct ath_softc *sc, bool retry_tx)
 	if (npend) {
 		int r;
 
-		ath_print(common, ATH_DBG_FATAL,
+		ath_print(common, ATH_DBG_XMIT,
 			  "Unable to stop TxDMA. Reset HAL!\n");
 
 		spin_lock_bh(&sc->sc_resetlock);
-		r = ath9k_hw_reset(ah, sc->sc_ah->curchan, false);
+		r = ath9k_hw_reset(ah, sc->sc_ah->curchan, true);
 		if (r)
 			ath_print(common, ATH_DBG_FATAL,
 				  "Unable to reset hardware; reset status %d\n",
