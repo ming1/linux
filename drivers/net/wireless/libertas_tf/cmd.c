26ca661e67d56e50b16d69cb378df23352911d3d
John W. Linville <linville@tuxdriver.com>
2010-04-28 16:20:47 -0400

    libertas_tf: avoid warning about pr_fmt redefinition
    Also includes a minor cleanup regarding quotation of a standard kernel
    header file...
    
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/libertas_tf/cmd.c
+++ drivers/net/wireless/libertas_tf/cmd.c
@@ -7,7 +7,7 @@
  *  the Free Software Foundation; either version 2 of the License, or (at
  *  your option) any later version.
  */
-#include "deb_defs.h"
+#define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
 
 #include <linux/slab.h>
 
