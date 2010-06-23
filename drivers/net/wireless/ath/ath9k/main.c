6667af2502d2ca8009909719b6ed69f46e80f785
John W. Linville <linville@tuxdriver.com>
2010-06-23 15:18:25 -0400

    Revert "ath9k: Add a module parameter to disable led blinking."
    This reverts commit 1e4d9acd679f97c0f1cc4ffbd04fa44bfc783b16.

--- drivers/net/wireless/ath/ath9k/main.c
+++ drivers/net/wireless/ath/ath9k/main.c
@@ -1241,9 +1241,7 @@ static void ath9k_stop(struct ieee80211_hw *hw)
 
 	aphy->state = ATH_WIPHY_INACTIVE;
 
-	if (led_blink)
-		cancel_delayed_work_sync(&sc->ath_led_blink_work);
-
+	cancel_delayed_work_sync(&sc->ath_led_blink_work);
 	cancel_delayed_work_sync(&sc->tx_complete_work);
 	cancel_work_sync(&sc->paprd_work);
 
