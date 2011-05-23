75596707f6c134bba5f79ff4fd0fb979b58c98c2
Paul Gortmaker <paul.gortmaker@windriver.com>
2011-05-23 14:51:41 -0400

    kernel: Map most files to use export.h instead of module.h
    The changed files were only including linux/module.h for the
    EXPORT_SYMBOL infrastructure, and nothing else.  Revector them
    onto the isolated export header for faster compile times.
    
    Nothing to see here but a whole lot of instances of:
    
      -#include <linux/module.h>
      +#include <linux/export.h>
    
    This commit is only changing the kernel dir; next targets
    will probably be mm, fs, the arch dirs, etc.
    
    Signed-off-by: Paul Gortmaker <paul.gortmaker@windriver.com>

--- kernel/exit.c
+++ kernel/exit.c
@@ -7,7 +7,7 @@
 #include <linux/mm.h>
 #include <linux/slab.h>
 #include <linux/interrupt.h>
-#include <linux/module.h>
+#include <linux/export.h>
 #include <linux/capability.h>
 #include <linux/completion.h>
 #include <linux/personality.h>
