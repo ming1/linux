e5b3105101754e526d5426cd9d2e22200ef9294b
Paul Gortmaker <paul.gortmaker@windriver.com>
2011-05-26 21:07:10 -0400

    kernel: params/rcutiny needs module.h not moduleparam.h
    Through various other twisted include paths, these files were
    getting the full module.h file, and hence living the illusion
    that it really only needed moduleparam.h -- but the reality
    is that once you remove module.h, you will get screenfulls of
    things like:
    
    kernel/params.c:583: warning: ‘struct module_kobject’ declared inside parameter list
    
    The file really requires module.h so simply make it so.  As the
    file module.h grabs moduleparam.h on the fly, all will be well.
    
    Signed-off-by: Paul Gortmaker <paul.gortmaker@windriver.com>

--- kernel/rcutiny.c
+++ kernel/rcutiny.c
@@ -22,7 +22,7 @@
  * For detailed explanation of Read-Copy Update mechanism see -
  *		Documentation/RCU
  */
-#include <linux/moduleparam.h>
+#include <linux/module.h>
 #include <linux/completion.h>
 #include <linux/interrupt.h>
 #include <linux/notifier.h>
