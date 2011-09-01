caf864c04abc6da1fd06ec588522e070595f5084
Stephen Rothwell <sfr@canb.auug.org.au>
2011-09-01 12:07:58 +1000

    Revert "Merge branch 'x86/spinlocks' into auto-latest"
    This reverts commit 6f8fa39c81f12d98540598db42a1eaff65bba0ce, reversing
    changes made to 4977f9bf2d9b511d36199ab3451a2592d6bc3793.

--- arch/x86/include/asm/spinlock_types.h
+++ arch/x86/include/asm/spinlock_types.h
@@ -5,29 +5,11 @@
 # error "please don't include this file directly"
 #endif
 
-#include <linux/types.h>
-
-#if (CONFIG_NR_CPUS < 256)
-typedef u8  __ticket_t;
-typedef u16 __ticketpair_t;
-#else
-typedef u16 __ticket_t;
-typedef u32 __ticketpair_t;
-#endif
-
-#define TICKET_SHIFT	(sizeof(__ticket_t) * 8)
-#define TICKET_MASK	((__ticket_t)((1 << TICKET_SHIFT) - 1))
-
 typedef struct arch_spinlock {
-	union {
-		__ticketpair_t head_tail;
-		struct __raw_tickets {
-			__ticket_t head, tail;
-		} tickets;
-	};
+	unsigned int slock;
 } arch_spinlock_t;
 
-#define __ARCH_SPIN_LOCK_UNLOCKED	{ { 0 } }
+#define __ARCH_SPIN_LOCK_UNLOCKED	{ 0 }
 
 #include <asm/rwlock.h>
 
