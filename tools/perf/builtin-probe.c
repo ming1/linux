e030f7af3ae82afc538a51907e1af7e83718b784
Srikar Dronamraju <srikar@linux.vnet.ibm.com>
2011-09-22 23:53:43 +0530

    perf: perf interface for uprobes
    Enhances perf probe to user space executables and libraries.
    Provides very basic support for uprobes.
    
    [ Probing a function in the executable using function name  ]
    -------------------------------------------------------------
    [root@localhost ~]# perf probe -x /bin/zsh zfree
    Add new event:
      probe_zsh:zfree      (on /bin/zsh:0x45400)
    
    You can now use it on all perf tools, such as:
    
    	perf record -e probe_zsh:zfree -aR sleep 1
    
    [root@localhost ~]# perf record -e probe_zsh:zfree -aR sleep 15
    [ perf record: Woken up 1 times to write data ]
    [ perf record: Captured and wrote 0.314 MB perf.data (~13715 samples) ]
    [root@localhost ~]# perf report --stdio
    # Events: 3K probe_zsh:zfree
    #
    # Overhead  Command  Shared Object  Symbol
    # ........  .......  .............  ......
    #
       100.00%              zsh  zsh            [.] zfree
    
    
    #
    # (For a higher level overview, try: perf report --sort comm,dso)
    #
    [root@localhost ~]
    
    [ Probing a library function using function name ]
    --------------------------------------------------
    [root@localhost]#
    [root@localhost]# perf probe -x /lib64/libc.so.6 malloc
    Add new event:
      probe_libc:malloc    (on /lib64/libc-2.5.so:0x74dc0)
    
    You can now use it on all perf tools, such as:
    
    	perf record -e probe_libc:malloc -aR sleep 1
    
    [root@localhost]#
    [root@localhost]# perf probe --list
      probe_libc:malloc    (on /lib64/libc-2.5.so:0x0000000000074dc0)
    
    Signed-off-by: Srikar Dronamraju <srikar@linux.vnet.ibm.com>

--- tools/perf/builtin-probe.c
+++ tools/perf/builtin-probe.c
@@ -57,6 +57,7 @@ static struct {
 	bool show_ext_vars;
 	bool show_funcs;
 	bool mod_events;
+	bool uprobes;
 	int nevents;
 	struct perf_probe_event events[MAX_PROBES];
 	struct strlist *dellist;
@@ -78,6 +79,7 @@ static int parse_probe_event(const char *str)
 		return -1;
 	}
 
+	pev->uprobes = params.uprobes;
 	/* Parse a perf-probe command into event */
 	ret = parse_perf_probe_command(str, pev);
 	pr_debug("%d arguments\n", pev->nargs);
@@ -126,6 +128,27 @@ static int opt_del_probe_event(const struct option *opt __used,
 		strlist__add(params.dellist, str);
 	}
 	return 0;
+}
+
+static int opt_set_target(const struct option *opt, const char *str,
+			int unset __used)
+{
+	int ret = -ENOENT;
+
+	if  (str && !params.target) {
+		if (!strcmp(opt->long_name, "exec"))
+			params.uprobes = true;
+#ifdef DWARF_SUPPORT
+		else if (!strcmp(opt->long_name, "module"))
+			params.uprobes = false;
+#endif
+		else
+			return ret;
+
+		params.target = str;
+		ret = 0;
+	}
+	return ret;
 }
 
 #ifdef DWARF_SUPPORT
@@ -249,9 +272,9 @@ static const struct option options[] = {
 		   "file", "vmlinux pathname"),
 	OPT_STRING('s', "source", &symbol_conf.source_prefix,
 		   "directory", "path to kernel source"),
-	OPT_STRING('m', "module", &params.target,
-		   "modname|path",
-		   "target module name (for online) or path (for offline)"),
+	OPT_CALLBACK('m', "module", NULL, "modname|path",
+		"target module name (for online) or path (for offline)",
+		opt_set_target),
 #endif
 	OPT__DRY_RUN(&probe_event_dry_run),
 	OPT_INTEGER('\0', "max-probes", &params.max_probe_points,
@@ -263,6 +286,8 @@ static const struct option options[] = {
 		     "\t\t\t(default: \"" DEFAULT_VAR_FILTER "\" for --vars,\n"
 		     "\t\t\t \"" DEFAULT_FUNC_FILTER "\" for --funcs)",
 		     opt_set_filter),
+	OPT_CALLBACK('x', "exec", NULL, "executable|path",
+			"target executable name or path", opt_set_target),
 	OPT_END()
 };
 
@@ -313,6 +338,10 @@ int cmd_probe(int argc, const char **argv, const char *prefix __used)
 			pr_err("  Error: Don't use --list with --funcs.\n");
 			usage_with_options(probe_usage, options);
 		}
+		if (params.uprobes) {
+			pr_warning("  Error: Don't use --list with --exec.\n");
+			usage_with_options(probe_usage, options);
+		}
 		ret = show_perf_probe_events();
 		if (ret < 0)
 			pr_err("  Error: Failed to show event list. (%d)\n",
@@ -346,7 +375,7 @@ int cmd_probe(int argc, const char **argv, const char *prefix __used)
 	}
 
 #ifdef DWARF_SUPPORT
-	if (params.show_lines) {
+	if (params.show_lines && !params.uprobes) {
 		if (params.mod_events) {
 			pr_err("  Error: Don't use --line with"
 			       " --add/--del.\n");
