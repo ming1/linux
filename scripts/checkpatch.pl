c9041edaefb31709f24f598c5d5b0c70567a6020
Stephen Rothwell <sfr@canb.auug.org.au>
2011-09-28 19:13:06 +1000

    Revert "kernel.h/checkpatch: mark strict_strto<foo> and simple_strto<foo> as obsolete"
    This reverts commit f43c361e2bd9f83222cedb9ba2f229345da3264d.

--- scripts/checkpatch.pl
+++ scripts/checkpatch.pl
@@ -3165,10 +3165,10 @@ sub process {
 			     "consider using a completion\n" . $herecurr);
 
 		}
-# recommend kstrto* over simple_strto* and strict_strto*
-		if ($line =~ /\b((simple|strict)_(strto(l|ll|ul|ull)))\s*\(/) {
+# recommend kstrto* over simple_strto*
+		if ($line =~ /\bsimple_(strto.*?)\s*\(/) {
 			WARN("CONSIDER_KSTRTO",
-			     "$1 is obsolete, use k$3 instead\n" . $herecurr);
+			     "consider using kstrto* in preference to simple_$1\n" . $herecurr);
 		}
 # check for __initcall(), use device_initcall() explicitly please
 		if ($line =~ /^.\s*__initcall\s*\(/) {
