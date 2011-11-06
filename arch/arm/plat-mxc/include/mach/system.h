77db1459d66d435d58daf70085f1456ad33377b3
Russell King <rmk+kernel@arm.linux.org.uk>
2011-11-06 17:12:08 +0000

    ARM: restart: mxc: use new restart hook
    Hook these platforms restart code into the new restart hook rather
    than using arch_reset().
    
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/plat-mxc/include/mach/system.h
+++ arch/arm/plat-mxc/include/mach/system.h
@@ -27,6 +27,8 @@ static inline void arch_idle(void)
 		cpu_do_idle();
 }
 
-void arch_reset(char mode, const char *cmd);
+static inline void arch_reset(char mode, const char *cmd)
+{
+}
 
 #endif /* __ASM_ARCH_MXC_SYSTEM_H__ */
