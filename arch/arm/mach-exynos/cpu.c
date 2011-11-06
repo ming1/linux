25767e6943a15d702bdc4cd7429f49f06ec6e4f4
Thomas Abraham <thomas.abraham@linaro.org>
2011-11-06 21:19:37 +0530

    ARM: EXYNOS: Add ioremap interceptor for statically remapped regions
    ioremap() request for statically remapped regions are intercepted
    and the statically assigned virtual address is returned. For requests
    for which there are no statically remapped regions, the requests are
    let through.
    
    Signed-off-by: Thomas Abraham <thomas.abraham@linaro.org>
    Signed-off-by: Kukjin Kim <kgene.kim@samsung.com>

--- arch/arm/mach-exynos/cpu.c
+++ arch/arm/mach-exynos/cpu.c
@@ -141,6 +141,28 @@ static struct map_desc exynos4_iodesc1[] __initdata = {
 	},
 };
 
+/*
+ * For all ioremap requests of statically mapped regions, intercept ioremap and
+ * return virtual address from the iodesc table.
+ */
+void __iomem *exynos4_ioremap(unsigned long phy, size_t size, unsigned int type)
+{
+	struct map_desc *desc;
+	unsigned int idx;
+
+	desc = exynos_iodesc;
+	for (idx = 0; idx < ARRAY_SIZE(exynos_iodesc); idx++, desc++)
+		if (desc->pfn == __phys_to_pfn(phy) && desc->type == type)
+			return (void __iomem *)desc->virtual;
+
+	desc = exynos4_iodesc;
+	for (idx = 0; idx < ARRAY_SIZE(exynos4_iodesc); idx++, desc++)
+		if (desc->pfn == __phys_to_pfn(phy) && desc->type == type)
+			return (void __iomem *)desc->virtual;
+
+	return __arm_ioremap(phy, size, type);
+}
+
 static void exynos_idle(void)
 {
 	if (!need_resched())
