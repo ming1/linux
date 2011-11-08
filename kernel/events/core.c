e1001382ba6a4767d5e47f1fccb91a072df6e684
Frederic Weisbecker <fweisbec@gmail.com>
2011-11-08 11:20:19 +1100

    cgroups: add previous cgroup in can_attach_task/attach_task callbacks
    This is to prepare the integration of a new max number of proc cgroup
    subsystem.  We'll need to release some resources from the previous cgroup.
    
    Signed-off-by: Frederic Weisbecker <fweisbec@gmail.com>
    Acked-by: Paul Menage <paul@paulmenage.org>
    Cc: Li Zefan <lizf@cn.fujitsu.com>
    Cc: Johannes Weiner <hannes@cmpxchg.org>
    Cc: Aditya Kali <adityakali@google.com>
    Cc: Oleg Nesterov <oleg@redhat.com>
    Cc: Kay Sievers <kay.sievers@vrfy.org>
    Cc: Tim Hockin <thockin@hockin.org>
    Cc: Tejun Heo <htejun@gmail.com>
    Acked-by: Kirill A. Shutemov <kirill@shutemov.name>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- kernel/events/core.c
+++ kernel/events/core.c
@@ -7047,7 +7047,8 @@ static int __perf_cgroup_move(void *info)
 }
 
 static void
-perf_cgroup_attach_task(struct cgroup *cgrp, struct task_struct *task)
+perf_cgroup_attach_task(struct cgroup *cgrp, struct cgroup *old_cgrp,
+			struct task_struct *task)
 {
 	task_function_call(task, __perf_cgroup_move, task);
 }
@@ -7063,7 +7064,7 @@ static void perf_cgroup_exit(struct cgroup_subsys *ss, struct cgroup *cgrp,
 	if (!(task->flags & PF_EXITING))
 		return;
 
-	perf_cgroup_attach_task(cgrp, task);
+	perf_cgroup_attach_task(cgrp, old_cgrp, task);
 }
 
 struct cgroup_subsys perf_subsys = {
