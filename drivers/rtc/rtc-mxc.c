3360e513ce52b4a4937d2d36be766d8aad6ccbac
Andrew Morton <akpm@linux-foundation.org>
2011-12-02 14:12:50 +1100

    drivers-rtc-rtc-mxcc-fix-setting-time-for-mx1-soc-fix
    use conventional comment layout
    
    Cc: Yauhen Kharuzhy <jekhor@gmail.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- drivers/rtc/rtc-mxc.c
+++ drivers/rtc/rtc-mxc.c
@@ -290,8 +290,8 @@ static int mxc_rtc_read_time(struct device *dev, struct rtc_time *tm)
  */
 static int mxc_rtc_set_mmss(struct device *dev, unsigned long time)
 {
-	/* RTC_DAYR register is 9-bit in MX1 SoC,
-	 * save time and day of year only
+	/*
+	 * TTC_DAYR register is 9-bit in MX1 SoC, save time and day of year only
 	 */
 	if (cpu_is_mx1()) {
 		struct rtc_time tm;
