e37d5961912ab36b3a48b207995b7e14071f7a63
Andrew Morton <akpm@linux-foundation.org>
2011-12-08 15:32:04 +1100

    cpusets-stall-when-updating-mems_allowed-for-mempolicy-or-disjoint-nodemask-fix
    stupid temporary hack to make it build with CONFIG_NUMA=n
    
    Cc: David Rientjes <rientjes@google.com>
    Cc: KOSAKI Motohiro <kosaki.motohiro@jp.fujitsu.com>
    Cc: Miao Xie <miaox@cn.fujitsu.com>
    Cc: Paul Menage <paul@paulmenage.org>
    Reported-by: Stephen Rothwell <sfr@canb.auug.org.au>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- kernel/cpuset.c
+++ kernel/cpuset.c
@@ -968,7 +968,10 @@ repeat:
 	 * tsk does not have a mempolicy, then an empty nodemask will not be
 	 * possible when mems_allowed is larger than a word.
 	 */
-	need_loop = tsk->mempolicy ||
+	need_loop =
+#ifdef CONFIG_NUMA
+		tsk->mempolicy ||
+#endif
 			!nodes_intersects(*newmems, tsk->mems_allowed);
 	nodes_or(tsk->mems_allowed, tsk->mems_allowed, *newmems);
 	mpol_rebind_task(tsk, newmems, MPOL_REBIND_STEP1);
