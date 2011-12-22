922a54248acd3c98f917b8fbd1b430d803ececdb
Andy Whitcroft <apw@canonical.com>
2011-12-22 16:04:25 +1100

    checkpatch: fix up complex macros context format
    Fix a missing newline and correctly use the raw context for the
    complex macro report.
    
    Signed-off-by: Andy Whitcroft <apw@canonical.com>
    Cc: Joe Perches <joe@perches.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- scripts/checkpatch.pl
+++ scripts/checkpatch.pl
@@ -2851,7 +2851,7 @@ sub process {
 					      "Macros with multiple statements should be enclosed in a do - while loop\n" . "$herectx");
 				} else {
 					ERROR("COMPLEX_MACRO",
-					      "Macros with complex values should be enclosed in parenthesis\n" . "$here$ctx");
+					      "Macros with complex values should be enclosed in parenthesis\n" . "$herectx");
 				}
 			}
 		}
