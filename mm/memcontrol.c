f3607ba55bb8a5359677bd1cd11a196a6534792c
Hugh Dickins <hughd@google.com>
2011-12-22 16:04:52 +1100

    memcg-clear-pc-mem_cgorup-if-necessary fix 3
    lookup_page_cgroup() returns NULL (or may return NULL) when
    mem_cgroup_disabled(): stop oops in mem_cgroup_reset_owner().  Let's make
    it explicit for now, probably optimize such checks later.
    
    Signed-off-by: Hugh Dickins <hughd@google.com>
    Cc: KAMEZAWA Hiroyuki <kamezawa.hiroyu@jp.fujitsu.com>
    Cc: Miklos Szeredi <mszeredi@suse.cz>
    Cc: Michal Hocko <mhocko@suse.cz>
    Cc: Johannes Weiner <hannes@cmpxchg.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- mm/memcontrol.c
+++ mm/memcontrol.c
@@ -3064,6 +3064,9 @@ void mem_cgroup_reset_owner(struct page *newpage)
 {
 	struct page_cgroup *pc;
 
+	if (mem_cgroup_disabled())
+		return;
+
 	pc = lookup_page_cgroup(newpage);
 	VM_BUG_ON(PageCgroupUsed(pc));
 	pc->mem_cgroup = root_mem_cgroup;
