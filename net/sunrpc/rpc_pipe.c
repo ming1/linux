51ce9ed2c8e5c5d3cffd8cba5edf51320423946a
Stanislav Kinsbursky <skinsbursky@parallels.com>
2011-12-26 15:39:30 +0300

    SUNRPC: put pipefs superblock link on network namespace
    We have modules (like, pNFS blocklayout module) which creates pipes on
    rpc_pipefs. Thus we need per-net operations for them. To make it possible we
    require appropriate super block. So we have to put sb link on network namespace
    context. Note, that it's not strongly required to create pipes in per-net
    operations. IOW, if pipefs wasn't mounted yet, that no sb link reference will
    present on network namespace and in this case we need just need to pass through
    pipe creation. Pipe dentry will be created during pipefs mount notification.
    
    Signed-off-by: Stanislav Kinsbursky <skinsbursky@parallels.com>

--- net/sunrpc/rpc_pipe.c
+++ net/sunrpc/rpc_pipe.c
@@ -1030,6 +1030,7 @@ rpc_fill_super(struct super_block *sb, void *data, int silent)
 	struct inode *inode;
 	struct dentry *root;
 	struct net *net = data;
+	struct sunrpc_net *sn = net_generic(net, sunrpc_net_id);
 	int err;
 
 	sb->s_blocksize = PAGE_CACHE_SIZE;
@@ -1054,6 +1055,7 @@ rpc_fill_super(struct super_block *sb, void *data, int silent)
 	if (err)
 		goto err_depopulate;
 	sb->s_fs_info = get_net(net);
+	sn->pipefs_sb = sb;
 	return 0;
 
 err_depopulate:
@@ -1074,7 +1076,9 @@ rpc_mount(struct file_system_type *fs_type,
 void rpc_kill_sb(struct super_block *sb)
 {
 	struct net *net = sb->s_fs_info;
+	struct sunrpc_net *sn = net_generic(net, sunrpc_net_id);
 
+	sn->pipefs_sb = NULL;
 	put_net(net);
 	blocking_notifier_call_chain(&rpc_pipefs_notifier_list,
 					   RPC_PIPEFS_UMOUNT,
