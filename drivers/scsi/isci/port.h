95dc5785d5da4e95773a6814762a728ceaa58a92
Dan Williams <dan.j.williams@intel.com>
2012-01-03 23:26:15 -0800

    isci: kill isci_port->status
    It only tracks whether the port is stopping in order to gate new devices
    being discovered while the port is stopping.  However, since the check
    and subsequent handling is unlocked there is nothing to stop the port
    from going down immediately after the check.
    
    Driver is already prepared to handle devices arriving on stale ports,
    and those will be cleaned up by an eventual ->lldd_dev_gone()
    notification.
    
    Signed-off-by: Dan Williams <dan.j.williams@intel.com>

--- drivers/scsi/isci/port.h
+++ drivers/scsi/isci/port.h
@@ -90,10 +90,8 @@ enum isci_status {
  * @timer: timeout start/stop operations
  */
 struct isci_port {
-	enum isci_status status;
 	struct isci_host *isci_host;
 	struct list_head remote_dev_list;
-	spinlock_t state_lock;
 	struct list_head domain_dev_list;
 	struct completion hard_reset_complete;
 	enum sci_status hard_reset_status;
@@ -283,9 +281,6 @@ void sci_port_get_sas_address(
 void sci_port_get_attached_sas_address(
 	struct isci_port *iport,
 	struct sci_sas_address *sas_address);
-
-enum isci_status isci_port_get_state(
-	struct isci_port *isci_port);
 
 void isci_port_formed(struct asd_sas_phy *);
 void isci_port_deformed(struct asd_sas_phy *);
