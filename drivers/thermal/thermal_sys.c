60f1281a1625d9e37778190ac9a8714c8908a54c
Fabio Estevam <festevam@gmail.com>
2012-02-01 12:00:11 +1100

    drivers/thermal/thermal_sys.c: fix build warning
    With CONFIG_NET=n:
    
    drivers/thermal/thermal_sys.c:63: warning: 'thermal_event_seqnum' defined but not used
    
    Move 'thermal_event_seqnum' definition inside the '#ifdef CONFIG_NET'
    
    Signed-off-by: Fabio Estevam <fabio.estevam@freescale.com>
    Acked-by: Guenter Roeck <guenter.roeck@ericsson.com>
    Acked-by: Durgadoss R <durgadoss.r@intel.com>
    Cc: Jean Delvare <khali@linux-fr.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- drivers/thermal/thermal_sys.c
+++ drivers/thermal/thermal_sys.c
@@ -60,8 +60,6 @@ static LIST_HEAD(thermal_tz_list);
 static LIST_HEAD(thermal_cdev_list);
 static DEFINE_MUTEX(thermal_list_lock);
 
-static unsigned int thermal_event_seqnum;
-
 static int get_idr(struct idr *idr, struct mutex *lock, int *id)
 {
 	int err;
@@ -1303,6 +1301,8 @@ static struct genl_family thermal_event_genl_family = {
 static struct genl_multicast_group thermal_event_mcgrp = {
 	.name = THERMAL_GENL_MCAST_GROUP_NAME,
 };
+
+static unsigned int thermal_event_seqnum;
 
 int thermal_generate_netlink_event(u32 orig, enum events event)
 {
