96d0edb68cb87c81baa591e4428edeb73f713f6c
Andrew Morton <akpm@linux-foundation.org>
2012-02-08 15:21:49 +1100

    video-backlight-support-s6e8ax0-panel-driver-based-on-mipi-dsi-checkpatch-fixes
    Cc: Donghwa Lee <dh09.lee@samsung.com>
    
    WARNING: please write a paragraph that describes the config symbol fully
    #32: FILE: drivers/video/exynos/Kconfig:16:
     config EXYNOS_MIPI_DSI
    
    WARNING: msleep < 20ms can sleep for up to 20ms; see Documentation/timers/timers-howto.txt
    +	msleep(1);
    WARNING: struct backlight_ops should normally be const
    #787: FILE: drivers/video/exynos/s6e8ax0.c:727:
    +static struct backlight_ops s6e8ax0_backlight_ops = {
    
    WARNING: msleep < 20ms can sleep for up to 20ms; see Documentation/timers/timers-howto.txt
    +	msleep(5);
    total: 0 errors, 4 warnings, 941 lines checked
    
    ./patches/video-backlight-support-s6e8ax0-panel-driver-based-on-mipi-dsi.patch has style problems, please review.
    
    If any of these errors are false positives, please report
    them to the maintainer, see CHECKPATCH in MAINTAINERS.
    
    Please run checkpatch prior to sending patches
    
    Cc: Donghwa Lee <dh09.lee@samsung.com>
    Cc: Florian Tobias Schandinat <FlorianSchandinat@gmx.de>
    Cc: Inki Dae <inki.dae@samsung.com>
    Cc: Jingoo Han <jg1.han@samsung.com>
    Cc: Kyungmin Park <kyungmin.park@samsung.com>
    Cc: Richard Purdie <rpurdie@rpsys.net>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- drivers/video/exynos/s6e8ax0.c
+++ drivers/video/exynos/s6e8ax0.c
@@ -724,7 +724,7 @@ static struct lcd_ops s6e8ax0_lcd_ops = {
 	.get_power = s6e8ax0_get_power,
 };
 
-static struct backlight_ops s6e8ax0_backlight_ops = {
+static const struct backlight_ops s6e8ax0_backlight_ops = {
 	.get_brightness = s6e8ax0_get_brightness,
 	.update_status = s6e8ax0_set_brightness,
 };
