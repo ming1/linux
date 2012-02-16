28b58c550afc55fc6d05296cf712dc788c42fe99
Andrew Morton <akpm@linux-foundation.org>
2012-02-17 10:59:05 +1100

    leds-lp5521-support-led-pattern-data-checkpatch-fixes
    Cc: "Kim, Milo" <Milo.Kim@ti.com>
    
    WARNING: strict_strtoul is obsolete, use kstrtoul instead
    #192: FILE: drivers/leds/leds-lp5521.c:616:
    +	ret = strict_strtoul(buf, 16, &val);
    
    total: 0 errors, 1 warnings, 194 lines checked
    
    ./patches/leds-lp5521-support-led-pattern-data.patch has style problems, please review.
    
    If any of these errors are false positives, please report
    them to the maintainer, see CHECKPATCH in MAINTAINERS.
    
    Please run checkpatch prior to sending patches
    
    Cc: "Kim, Milo" <Milo.Kim@ti.com>
    Cc: Arun MURTHY <arun.murthy@stericsson.com>
    Cc: Linus Walleij <linus.walleij@linaro.org>
    Cc: Milo(Woogyom) Kim <milo.kim@ti.com>
    Cc: Richard Purdie <rpurdie@rpsys.net>
    Cc: Srinidhi Kasagar <srinidhi.kasagar@stericsson.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- drivers/leds/leds-lp5521.c
+++ drivers/leds/leds-lp5521.c
@@ -496,7 +496,7 @@ static ssize_t store_current(struct device *dev,
 	ssize_t ret;
 	unsigned long curr;
 
-	if (strict_strtoul(buf, 0, &curr))
+	if (kstrtoul(buf, 0, &curr))
 		return -EINVAL;
 
 	if (curr > led->max_current)
