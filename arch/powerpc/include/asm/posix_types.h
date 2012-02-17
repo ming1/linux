bb903a3a7b870b7665123d121f77c1a7218ae813
Stephen Rothwell <sfr@canb.auug.org.au>
2012-02-17 16:25:48 +1100

    powerpc: use generic posix_types.h fix
    Signed-off-by: Stephen Rothwell <sfr@canb.auug.org.au>

--- arch/powerpc/include/asm/posix_types.h
+++ arch/powerpc/include/asm/posix_types.h
@@ -12,7 +12,7 @@ typedef unsigned long	__kernel_old_dev_t;
 #define __kernel_old_dev_t __kernel_old_dev_t
 #else
 typedef unsigned int	__kernel_size_t;
-typedef int		__kernel_size_t;
+typedef int		__kernel_ssize_t;
 typedef long		__kernel_ptrdiff_t;
 #define __kernel_size_t __kernel_size_t
 
