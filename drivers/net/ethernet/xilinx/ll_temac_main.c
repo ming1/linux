d6e4ac700308b6e08a28cd10be6a222117c95860
Stephen Rothwell <sfr@canb.auug.org.au>
2012-02-20 16:15:27 +1100

    Revert "xilinx ll_temac: use eth_hw_addr_random() instead of random_ether_addr()"
    This reverts commit 5a5e7c36448664a58d13aab5a9006b01f6a58910.

--- drivers/net/ethernet/xilinx/ll_temac_main.c
+++ drivers/net/ethernet/xilinx/ll_temac_main.c
@@ -327,9 +327,7 @@ static int temac_set_mac_address(struct net_device *ndev, void *address)
 		memcpy(ndev->dev_addr, address, ETH_ALEN);
 
 	if (!is_valid_ether_addr(ndev->dev_addr))
-		eth_hw_addr_random(ndev);
-	else
-		dev->addr_assign_type &= ~NET_ADDR_RANDOM;
+		random_ether_addr(ndev->dev_addr);
 
 	/* set up unicast MAC address filter set its mac address */
 	mutex_lock(&lp->indirect_mutex);
