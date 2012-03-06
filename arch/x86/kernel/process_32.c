9db62b386ae033c8d15a69f271f3f43e900fc97b
Alex Shi <alex.shi@intel.com>
2012-03-06 11:16:34 +1100

    x86: use this_cpu_xxx to replace percpu_xxx funcs
    Since percpu_xxx() serial functions are duplicate with this_cpu_xxx().
    Removing percpu_xxx() definition and replacing them by this_cpu_xxx() in
    code.
    
    And further more, as Christoph Lameter's requirement, I try to use
    __this_cpu_xx to replace this_cpu_xxx if it is in preempt safe scenario.
    The preempt safe scenarios include:
    1, in irq/softirq/nmi handler
    2, protected by preempt_disable
    3, protected by spin_lock
    4, if the code context imply that it is preempt safe, like the code is
    follows or be followed a preempt safe code.
    
    BTW, In fact, this_cpu_xxx are same as __this_cpu_xxx since all funcs
    implement in a single instruction for x86 machine.  But it maybe other
    platforms' performance.
    
    [akpm@linux-foundation.org: fix build]
    [sfr@canb.auug.org.au: arch/x86/include/asm/desc.h: fix smp_processor_id's need for this_cpu_read]
    Signed-off-by: Alex Shi <alex.shi@intel.com>
    Acked-by: Christoph Lameter <cl@gentwo.org>
    Acked-by: Tejun Heo <tj@kernel.org>
    Cc: Ingo Molnar <mingo@elte.hu>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: "H. Peter Anvin" <hpa@zytor.com>
    Signed-off-by: Stephen Rothwell <sfr@canb.auug.org.au>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- arch/x86/kernel/process_32.c
+++ arch/x86/kernel/process_32.c
@@ -360,7 +360,7 @@ __switch_to(struct task_struct *prev_p, struct task_struct *next_p)
 
 	switch_fpu_finish(next_p, fpu);
 
-	percpu_write(current_task, next_p);
+	__this_cpu_write(current_task, next_p);
 
 	return prev_p;
 }
