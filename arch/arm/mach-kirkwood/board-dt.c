5e40593b5aa214b502ce057be7e96c4a697509f2
Jason Cooper <jason@lakedaemon.net>
2012-03-06 23:55:04 +0000

    ARM: kirkwood: use devicetree for rtc-mv
    Signed-off-by: Jason Cooper <jason@lakedaemon.net>

--- arch/arm/mach-kirkwood/board-dt.c
+++ arch/arm/mach-kirkwood/board-dt.c
@@ -181,7 +181,6 @@ static void __init kirkwood_dt_init(void)
 		irq_domain_add_simple(node, 0);
 
 	/* internal devices that every board has */
-	kirkwood_rtc_init();
 	kirkwood_wdt_init();
 	kirkwood_xor0_init();
 	kirkwood_xor1_init();
