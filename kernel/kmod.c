f3acff8879d26d2218ebfc010ec6e6fa43ca9751
Tetsuo Handa <penguin-kernel@i-love.sakura.ne.jp>
2012-03-06 11:25:35 +1100

    kmod: avoid deadlock from recursive kmod call
    The system deadlocks (at least since 2.6.10) when
    call_usermodehelper(UMH_WAIT_EXEC) request triggered
    call_usermodehelper(UMH_WAIT_PROC) request.
    
    This is because "khelper thread is waiting for the worker thread at
    wait_for_completion() in do_fork() since the worker thread was created
    with CLONE_VFORK flag" and "the worker thread cannot call complete()
    because do_execve() is blocked at UMH_WAIT_PROC request" and "the khelper
    thread cannot start processing UMH_WAIT_PROC request because the khelper
    thread is waiting for the worker thread at wait_for_completion() in
    do_fork()".
    
    In order to avoid deadlock, do not try to call wait_for_completion() in
    call_usermodehelper_exec() if the worker thread was created by khelper
    thread with CLONE_VFORK flag.
    
    The easiest example to observe this deadlock is to use a corrupted
    /sbin/hotplug binary (like shown below).
    
      # : > /tmp/dummy
      # chmod 755 /tmp/dummy
      # echo /tmp/dummy > /proc/sys/kernel/hotplug
      # modprobe whatever
    
    call_usermodehelper("/tmp/dummy", UMH_WAIT_EXEC) is called from
    kobject_uevent_env() in lib/kobject_uevent.c upon loading/unloading a
    module.  do_execve("/tmp/dummy") triggers a call to
    request_module("binfmt-0000") from search_binary_handler() which in turn
    calls call_usermodehelper(UMH_WAIT_PROC).
    
    There are various hooks called during do_execve() operation (e.g.
    security_bprm_check(), audit_bprm(), "struct
    linux_binfmt"->load_binary()).  If one of such hooks triggers
    UMH_WAIT_EXEC, this deadlock will happen even if /sbin/hotplug is not
    corrupted.
    
    Signed-off-by: Tetsuo Handa <penguin-kernel@I-love.SAKURA.ne.jp>
    Cc: Arjan van de Ven <arjan@linux.intel.com>
    Cc: Rusty Russell <rusty@rustcorp.com.au>
    Cc: Tejun Heo <tj@kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- kernel/kmod.c
+++ kernel/kmod.c
@@ -44,6 +44,7 @@
 extern int max_threads;
 
 static struct workqueue_struct *khelper_wq;
+static const struct task_struct *kmod_thread_locker;
 
 #define CAP_BSET	(void *)1
 #define CAP_PI		(void *)2
@@ -221,6 +222,13 @@ fail:
 	return 0;
 }
 
+static int call_helper(void *data)
+{
+	/* Worker thread started blocking khelper thread. */
+	kmod_thread_locker = current;
+	return ____call_usermodehelper(data);
+}
+
 void call_usermodehelper_freeinfo(struct subprocess_info *info)
 {
 	if (info->cleanup)
@@ -296,9 +304,12 @@ static void __call_usermodehelper(struct work_struct *work)
 	if (wait == UMH_WAIT_PROC)
 		pid = kernel_thread(wait_for_helper, sub_info,
 				    CLONE_FS | CLONE_FILES | SIGCHLD);
-	else
-		pid = kernel_thread(____call_usermodehelper, sub_info,
+	else {
+		pid = kernel_thread(call_helper, sub_info,
 				    CLONE_VFORK | SIGCHLD);
+		/* Worker thread stopped blocking khelper thread. */
+		kmod_thread_locker = NULL;
+	}
 
 	switch (wait) {
 	case UMH_NO_WAIT:
@@ -488,6 +499,16 @@ int call_usermodehelper_exec(struct subprocess_info *sub_info, int wait)
 		goto out;
 
 	if (!khelper_wq || usermodehelper_disabled) {
+		retval = -EBUSY;
+		goto out;
+	}
+	/*
+	 * Worker thread must not wait for khelper thread at below
+	 * wait_for_completion() if the thread was created with CLONE_VFORK
+	 * flag, for khelper thread is already waiting for the thread at
+	 * wait_for_completion() in do_fork().
+	 */
+	if (wait != UMH_NO_WAIT && current == kmod_thread_locker) {
 		retval = -EBUSY;
 		goto out;
 	}
