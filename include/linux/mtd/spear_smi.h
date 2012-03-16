1a51f2932191763c6e87e89d148d0cb69eed6e0b
Stefan Roese <sr@denx.de>
2012-03-16 11:42:11 +0100

    mtd: add device-tree support to spear_smi
    This patch adds support to configure the SPEAr SMI driver via
    device-tree instead of platform_data.
    
    Signed-off-by: Stefan Roese <sr@denx.de>
    Signed-off-by: Artem Bityutskiy <artem.bityutskiy@linux.intel.com>

--- include/linux/mtd/spear_smi.h
+++ include/linux/mtd/spear_smi.h
@@ -16,6 +16,10 @@
 #include <linux/mtd/mtd.h>
 #include <linux/mtd/partitions.h>
 #include <linux/platform_device.h>
+#include <linux/of.h>
+
+/* max possible slots for serial-nor flash chip in the SMI controller */
+#define MAX_NUM_FLASH_CHIP	4
 
 /* macro to define partitions for flash devices */
 #define DEFINE_PARTS(n, of, s)		\
@@ -57,6 +61,7 @@ struct spear_smi_plat_data {
 	unsigned long clk_rate;
 	int num_flashes;
 	struct spear_smi_flash_info *board_flash_info;
+	struct device_node *np[MAX_NUM_FLASH_CHIP];
 };
 
 #endif /* __MTD_SPEAR_SMI_H */
