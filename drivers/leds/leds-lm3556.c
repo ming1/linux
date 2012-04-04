d13a92bf15b9ad0d56f55541b5a9a9f9096e1b4a
Andrew Morton <akpm@linux-foundation.org>
2012-04-04 10:08:29 +1000

    leds-add-led-driver-for-lm3556-chip-fix-2
    include module.h
    
    Cc: Axel Lin <axel.lin@gmail.com>
    Cc: Daniel Jeong <daniel.jeong@ti.com>
    Cc: Geon Si Jeong <gshark.jeong@gmail.com>
    Cc: Richard Purdie <rpurdie@rpsys.net>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- drivers/leds/leds-lm3556.c
+++ drivers/leds/leds-lm3556.c
@@ -11,6 +11,7 @@
 #include <linux/delay.h>
 #include <linux/i2c.h>
 #include <linux/leds.h>
+#include <linux/module.h>
 #include <linux/slab.h>
 #include <linux/platform_device.h>
 #include <linux/types.h>
