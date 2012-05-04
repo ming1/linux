7fedfad2ac40f43ac49cb669a8b0fd6b1f006bb1
Nicholas Bellinger <nab@linux-iscsi.org>
2012-05-03 18:55:57 -0700

    qla2xxx: Enable >= 24xx target-mode support in SCSI LLD
    *) Addition of target mode specific members to existing data
    structures in qla_def.h and struct qla_hw_data->tgt_ops using
    qla_target.h:struct qla_tgt_func_tmpl
    
    *) Addition of struct qla_tgt_func_tmpl and direct calls into
    qla_target.c logic w/ qlt_* prefixed functions.
    
    *) Addition of qla_iocb.c:qla2x00_req_pkt() for ring processing, and
    qla2x00_issue_marker() for handling request/response queue processing
    for target mode operation
    
    *) Addition of various qla_tgt_mode_enabled() logic checks in
    qla24xx_nvram_config(), qla2x00_initialize_adapter(), qla2x00_rff_id(),
    qla2x00_abort_isp(), qla24xx_modify_vp_config(), and
    qla2x00_vp_abort_isp().
    
    More specific checks for qla_hw_data->qla2x_tmpl include:
    
    *) control plane:
    
    qla_init.c:qla2x00_rport_del() -> qlt_fc_port_deleted()
    qla_init.c:qla2x00_reg_remote_port() -> qlt_fc_port_added()
    
    *) I/O path:
    
    qla_isr.c:qla2x00_async_event() -> qlt_async_event()
    qla_isr.c:qla2x00_process_response_queue() ->
    qlt_response_pkt_all_vps()
    qla_isr.c:qla24xx_process_response_queue() ->
    qlt_response_pkt_all_vps()
    
    *) interrupt handlers:
    
    qla_isr.c:qla24xx_intr_handler() -> qlt_24xx_process_atio_queue() +
                                        qla24xx_process_response_queue()
    qla24xx_msix_default(): qlt_24xx_process_atio_queue() +
                            qla24xx_process_response_queue()
    
    (nab: Drop legacy qla_init.c:qla2x00_device_resync() target usage +
          qla2xxx: Fix port database pointer ref for !IS_FWI2_CAPABLE (DanC) +
          drop qla2x00_get_port_database() for changes fcport->port_type assignment +
          update qla_tgt_handle_srr_work for v3.5 target usage)
    (arun: Target mode review comments for v3.5 +
           Use helper instead of direct access of tgt_vp_map)
    (roland: Don't create duplicate target sessions to address tearing down
             ACLs with IO in flight)
    
    Cc: Andrew Vasquez <andrew.vasquez@qlogic.com>
    Cc: Arun Easi <arun.easi@qlogic.com>
    Cc: Giridhar Malavali <giridhar.malavali@qlogic.com>
    Cc: Christoph Hellwig <hch@lst.de>
    Cc: James Bottomley <JBottomley@Parallels.com>
    Cc: Roland Dreier <roland@purestorage.com>
    Cc: Joern Engel <joern@logfs.org>
    Cc: Madhuranath Iyengar <mni@risingtidesystems.com>
    Signed-off-by: Nicholas A. Bellinger <nab@linux-iscsi.org>
    
    qla2xxx: Use helper instead of direct access of tgt_vp_map.
    
    tgt_vp_map will not be allocated if target mode is not enabled, so
    access to that has to be via the helper routine.
    
    Signed-off-by: Arun Easi <arun.easi@qlogic.com>
    Signed-off-by: Nicholas Bellinger <nab@linux-iscsi.org>

--- drivers/scsi/qla2xxx/qla_dbg.h
+++ drivers/scsi/qla2xxx/qla_dbg.h
@@ -244,6 +244,7 @@ struct qla2xxx_mqueue_header {
 	uint32_t queue;
 #define TYPE_REQUEST_QUEUE	0x1
 #define TYPE_RESPONSE_QUEUE	0x2
+#define TYPE_ATIO_QUEUE		0x3
 	uint32_t number;
 	uint32_t size;
 };
@@ -339,3 +340,6 @@ ql_log_pci(uint32_t, struct pci_dev *pdev, int32_t, const char *fmt, ...);
 #define ql_dbg_misc	0x00010000 /* For dumping everything that is not
 				    * not covered by upper categories
 				    */
+#define ql_dbg_tgt	0x00004000 /* Target mode */
+#define ql_dbg_tgt_mgt	0x00002000 /* Target mode management */
+#define ql_dbg_tgt_tmr	0x00001000 /* Target mode task management */
