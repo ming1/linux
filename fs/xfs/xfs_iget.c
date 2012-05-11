c71d668a378cf96036e02e7c0244354319b8b825
Andi Kleen <ak@linux.intel.com>
2012-05-11 10:58:54 +1000

    XFS: fix lock ASSERT on UP
    ASSERT(!spin_is_locked()) doesn't work on UP builds.  Replace with a
    standard lockdep_assert_held()
    
    Signed-off-by: Andi Kleen <ak@linux.intel.com>
    Cc: Ben Myers <bpm@sgi.com>
    Cc: Alex Elder <elder@kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- fs/xfs/xfs_iget.c
+++ fs/xfs/xfs_iget.c
@@ -75,7 +75,7 @@ xfs_inode_alloc(
 	}
 
 	ASSERT(atomic_read(&ip->i_pincount) == 0);
-	ASSERT(!spin_is_locked(&ip->i_flags_lock));
+	lockdep_assert_held(&ip->i_flags_lock);
 	ASSERT(!xfs_isiflocked(ip));
 	ASSERT(ip->i_ino == 0);
 
@@ -129,7 +129,7 @@ xfs_inode_free(
 
 	/* asserts to verify all state is correct here */
 	ASSERT(atomic_read(&ip->i_pincount) == 0);
-	ASSERT(!spin_is_locked(&ip->i_flags_lock));
+	lockdep_assert_held(&ip->i_flags_lock);
 	ASSERT(!xfs_isiflocked(ip));
 
 	/*
