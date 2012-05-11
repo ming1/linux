7d7c3f4fe9ba96b25c77337b38ddad9774041254
Andrew Morton <akpm@linux-foundation.org>
2012-05-11 10:58:31 +1000

    mm-memcg-count-pte-references-from-every-member-of-the-reclaimed-hierarchy-fix
    name the args in the declaration
    
    Cc: Johannes Weiner <hannes@cmpxchg.org>
    Cc: Konstantin Khlebnikov <khlebnikov@openvz.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- include/linux/memcontrol.h
+++ include/linux/memcontrol.h
@@ -79,7 +79,8 @@ extern void mem_cgroup_uncharge_cache_page(struct page *page);
 
 extern void mem_cgroup_out_of_memory(struct mem_cgroup *memcg, gfp_t gfp_mask,
 				     int order);
-bool __mem_cgroup_same_or_subtree(const struct mem_cgroup *, struct mem_cgroup *);
+bool __mem_cgroup_same_or_subtree(const struct mem_cgroup *root_memcg,
+				  struct mem_cgroup *memcg);
 int task_in_mem_cgroup(struct task_struct *task, const struct mem_cgroup *memcg);
 
 extern struct mem_cgroup *try_get_mem_cgroup_from_page(struct page *page);
