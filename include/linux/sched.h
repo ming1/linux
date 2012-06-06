e49c5d0eed1dcb3f5d94d8addb6d0e587c308fd5
Andrew Morton <akpm@linux-foundation.org>
2012-06-07 08:49:06 +1000

    nmi-watchdog-fix-for-lockup-detector-breakage-on-resume-fix-fix
    make lockup_detector_bootcpu_resume() conditional on CONFIG_SUSPEND
    
    Cc: "Rafael J. Wysocki" <rjw@sisk.pl>
    Cc: Don Zickus <dzickus@redhat.com>
    Cc: Ingo Molnar <mingo@elte.hu>
    Cc: Mandeep Singh Baines <msb@chromium.org>
    Cc: Peter Zijlstra <a.p.zijlstra@chello.nl>
    Cc: Sameer Nanda <snanda@chromium.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -320,7 +320,6 @@ extern int proc_dowatchdog_thresh(struct ctl_table *table, int write,
 				  size_t *lenp, loff_t *ppos);
 extern unsigned int  softlockup_panic;
 void lockup_detector_init(void);
-void lockup_detector_bootcpu_resume(void);
 #else
 static inline void touch_softlockup_watchdog(void)
 {
@@ -334,6 +333,11 @@ static inline void touch_all_softlockup_watchdogs(void)
 static inline void lockup_detector_init(void)
 {
 }
+#endif
+
+#if defined(CONFIG_LOCKUP_DETECTOR) && defined(CONFIG_SUSPEND)
+void lockup_detector_bootcpu_resume(void);
+#else
 static inline void lockup_detector_bootcpu_resume(void)
 {
 }
