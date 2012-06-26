705dd8ba826cefd0d32d2ff7d29e2af4fdf0350e
Shawn Guo <shawn.guo@linaro.org>
2012-06-26 16:38:57 +0800

    mmc: mxs-mmc: enable regulator for mmc slot
    If bootloader or platform initialization code does not enable the
    power supply to mmc slot, we need to do it in mmc driver.
    
    Signed-off-by: Shawn Guo <shawn.guo@linaro.org>
    Signed-off-by: Chris Ball <cjb@laptop.org>

--- drivers/mmc/host/sdhci-pci.c
+++ drivers/mmc/host/sdhci-pci.c
@@ -652,6 +652,10 @@ static const struct sdhci_pci_fixes sdhci_via = {
 	.probe		= via_probe,
 };
 
+static const struct sdhci_pci_fixes sdhci_xo1 = {
+	.quirks		= SDHCI_QUIRK_BROKEN_CARD_DETECTION,
+};
+
 static const struct pci_device_id pci_ids[] __devinitdata = {
 	{
 		.vendor		= PCI_VENDOR_ID_RICOH,
@@ -891,6 +895,14 @@ static const struct pci_device_id pci_ids[] __devinitdata = {
 		.subvendor	= PCI_ANY_ID,
 		.subdevice	= PCI_ANY_ID,
 		.driver_data	= (kernel_ulong_t)&sdhci_o2,
+	},
+
+	{
+		.vendor		= PCI_VENDOR_ID_MARVELL,
+		.device		= PCI_DEVICE_ID_MARVELL_88ALP01_SD,
+		.subvendor	= PCI_ANY_ID,
+		.subdevice	= PCI_ANY_ID,
+		.driver_data    = (kernel_ulong_t)&sdhci_xo1,
 	},
 
 	{	/* Generic SD host controller */
