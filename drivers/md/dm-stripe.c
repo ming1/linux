3c5363894a52399fcc47a01bf9d27cd6b13d95cd
Mikulas Patocka <mpatocka@redhat.com>
2012-07-10 10:41:50 +1000

    dm-stripe is supposed to ensure that all the space allocated to the stripes is fully used and that all stripes are the same size.  This patch fixes the test.  It checks that device length is divisible by the chunk size and checks that the resulting quotient is divisible by the number of stripes (which is equivalent to testing if device length is divisible by chunk_size * stripes).
    Previously, the code only tested that the number of sectors in the target
    was divisible by each of the chunk size and the number of stripes
    separately, which could leave entire stripes unused.
    
    (A setup that genuinely needs some stripes to be shorter than others
    can be created by concatenating striped targets.)
    
    Signed-off-by: Mikulas Patocka <mpatocka@redhat.com>
    Signed-off-by: Alasdair G Kergon <agk@redhat.com>

--- drivers/md/dm-stripe.c
+++ drivers/md/dm-stripe.c
@@ -135,7 +135,6 @@ static int stripe_ctr(struct dm_target *ti, unsigned int argc, char **argv)
 		return -EINVAL;
 	}
 
-	width = ti->len;
 	if (sector_div(width, stripes)) {
 		ti->error = "Target length not divisible by "
 		    "number of stripes";
