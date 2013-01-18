52b5a9aff59ecf30bdae4bcb616482a8f66fea72
Vineet Gupta <vgupta@synopsys.com>
2013-01-18 15:12:19 +0530

    ARC: Signal handling
    Includes following fixes courtesy review by Al-Viro
    
    * Tracer poke to Callee-regs were lost
    
      Before going off into do_signal( ) we save the user-mode callee regs
      (as they are not saved by default as part of pt_regs). This is to make
      sure that that a Tracer (if tracing related signal) is able to do likes
      of PEEKUSR(callee-reg).
    
      However in return path we were simply discarding the user-mode callee
      regs, which would break a POKEUSR(callee-reg) from a tracer.
    
    * Issue related to multiple syscall restarts are addressed in next patch
    
    Signed-off-by: Vineet Gupta <vgupta@synopsys.com>
    Cc: Al Viro <viro@ZenIV.linux.org.uk>

---
+++ arch/arc/include/asm/sigcontext.h
@@ -0,0 +1,23 @@
+/*
+ * Copyright (C) 2004, 2007-2010, 2011-2012 Synopsys, Inc. (www.synopsys.com)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+
+#ifndef _ASM_ARC_SIGCONTEXT_H
+#define _ASM_ARC_SIGCONTEXT_H
+
+#include <asm/ptrace.h>
+
+/*
+ * Signal context structure - contains all info to do with the state
+ * before the signal handler was invoked.  Note: only add new entries
+ * to the end of the structure.
+ */
+struct sigcontext {
+	struct pt_regs regs;
+};
+
+#endif /* _ASM_ARC_SIGCONTEXT_H */
