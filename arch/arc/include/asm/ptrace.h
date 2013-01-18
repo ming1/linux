ff71ed51c283a5ced30aac99130d5d90dae479fb
Vineet Gupta <vgupta@synopsys.com>
2013-01-18 15:12:25 +0530

    ARC: UAPI Disintegrate arch/arc/include/asm
    1. ./genfilelist.pl arch/arc/include/asm/
    
    2. Create arch/arc/include/uapi/asm/Kbuild as follows
    
    	+# UAPI Header export list
    	+include include/uapi/asm-generic/Kbuild.asm
    
    3. ./disintegrate-one.pl arch/arc/include/{,uapi/}asm/<above-list>
    
    4. Edit arch/arc/include/asm/Kbuild to remove ref to
    	asm-generic/Kbuild.asm
    
    To work around empty uapi/asm/setup.h added a placholder comment.
    
    Signed-off-by: Vineet Gupta <vgupta@synopsys.com>
    Cc: David Howells <dhowells@redhat.com>

--- arch/arc/include/asm/ptrace.h
+++ arch/arc/include/asm/ptrace.h
@@ -7,42 +7,11 @@
  *
  * Amit Bhor, Sameer Dhavale: Codito Technologies 2004
  */
-
 #ifndef __ASM_ARC_PTRACE_H
 #define __ASM_ARC_PTRACE_H
 
-/*
- * XXX: ABI hack.
- * The offset calc can be cleanly done in asm-offset.c, however gdb includes
- * this header directly.
- */
-#define PT_bta		4
-#define PT_lp_start	8
-#define PT_lp_end	12
-#define PT_lp_count	16
-#define PT_status32	20
-#define PT_ret		24
-#define PT_blink	28
-#define PT_fp		32
-#define PT_r26		36
-#define PT_r12		40
-#define PT_r11		44
-#define PT_r10		48
-#define PT_r9		52
-#define PT_r8		56
-#define PT_r7		60
-#define PT_r6		64
-#define PT_r5		68
-#define PT_r4		72
-#define PT_r3		76
-#define PT_r2		80
-#define PT_r1		84
-#define PT_r0		88
-#define PT_sp		92
-#define PT_orig_r0	96
-#define PT_orig_r8	100
+#include <uapi/asm/ptrace.h>
 
-#ifdef __KERNEL__
 
 #ifndef __ASSEMBLY__
 
@@ -163,7 +132,5 @@ static inline long regs_return_value(struct pt_regs *regs)
 #define orig_r8_IS_EXCPN		0x0004
 #define orig_r8_IS_IRQ1			0x0010
 #define orig_r8_IS_IRQ2			0x0020
-
-#endif /* __KERNEL__ */
 
 #endif /* __ASM_PTRACE_H */
