4ec3365d1aa04a28a17d51e4e4629ee3d591c8e0
Kim, Milo <Milo.Kim@ti.com>
2013-01-23 15:43:41 -0800

    leds-lp5521/3: use '%zu' for formatting 'size_t'
    The printk format of type 'size_t' is not '%d' but '%zu' or '%zx'.
    This patch fixes build warnings below.
    
     drivers/leds/leds-lp5521.c: In function lp5521_firmware_loaded:
     drivers/leds/leds-lp5521.c:257:4: warning: format %d expects argument of
      type int, but argument 3 has type size_t [-Wformat]
    
     drivers/leds/leds-lp5523.c: In function lp5523_firmware_loaded:
     drivers/leds/leds-lp5523.c:283:4: warning: format %d expects argument of
      type int, but argument 3 has type size_t [-Wformat]
    
    Signed-off-by: Milo(Woogyom) Kim <milo.kim@ti.com>
    Signed-off-by: Bryan Wu <cooloney@gmail.com>

--- drivers/leds/leds-lp5523.c
+++ drivers/leds/leds-lp5523.c
@@ -279,7 +279,7 @@ static void lp5523_firmware_loaded(struct lp55xx_chip *chip)
 	const struct firmware *fw = chip->fw;
 
 	if (fw->size > LP5523_PROGRAM_LENGTH) {
-		dev_err(&chip->cl->dev, "firmware data size overflow: %d\n",
+		dev_err(&chip->cl->dev, "firmware data size overflow: %zu\n",
 			fw->size);
 		return;
 	}
