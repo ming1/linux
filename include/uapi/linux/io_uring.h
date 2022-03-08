cdf7d43e7ee3a499b9187b087b6e10c15b64d7a8
Jens Axboe <axboe@kernel.dk>
2022-03-08 20:50:49 +0530

    io_uring: add support for 128-byte SQEs
    Normal SQEs are 64-bytes in length, which is fine for all the commands
    we support. However, in preparation for supporting passthrough IO,
    provide an option for setting up a ring with 128-byte SQEs.
    
    We continue to use the same type for io_uring_sqe, it's marked and
    commented with a zero sized array pad at the end. This provides up
    to 80 bytes of data for a passthrough command - 64 bytes for the
    extra added data, and 16 bytes available at the end of the existing
    SQE.
    
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- include/uapi/linux/io_uring.h
+++ include/uapi/linux/io_uring.h
@@ -61,6 +61,12 @@ struct io_uring_sqe {
 		__u32	file_index;
 	};
 	__u64	__pad2[2];
+
+	/*
+	 * If the ring is initializefd with IORING_SETUP_SQE128, then this field
+	 * contains 64-bytes of padding, doubling the size of the SQE.
+	 */
+	__u64	__big_sqe_pad[0];
 };
 
 enum {
@@ -101,6 +107,7 @@ enum {
 #define IORING_SETUP_CLAMP	(1U << 4)	/* clamp SQ/CQ ring sizes */
 #define IORING_SETUP_ATTACH_WQ	(1U << 5)	/* attach to existing wq */
 #define IORING_SETUP_R_DISABLED	(1U << 6)	/* start with ring disabled */
+#define IORING_SETUP_SQE128	(1U << 7)	/* SQEs are 128b */
 
 enum {
 	IORING_OP_NOP,
