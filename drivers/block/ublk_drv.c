25055166c5ba96a525cc2e645919bd4b769c7cb3
Ming Lei <ming.lei@redhat.com>
2025-05-27 15:02:25 +0000

    ublk: don't track task context for command batch
    batch io is designed to be independent of task context, so don't
    track task context for batch io feature.
    
    Signed-off-by: Ming Lei <ming.lei@redhat.com>

--- drivers/block/ublk_drv.c
+++ drivers/block/ublk_drv.c
@@ -247,6 +247,11 @@ static inline bool ublk_dev_support_batch_io(const struct ublk_device *ub)
 	return false;
 }
 
+static inline bool ublk_support_batch_io(const struct ublk_queue *ubq)
+{
+	return false;
+}
+
 static inline struct ublksrv_io_desc *
 ublk_get_iod(const struct ublk_queue *ubq, unsigned tag)
 {
@@ -1297,6 +1302,8 @@ static void ublk_dispatch_req(struct ublk_queue *ubq,
 			__func__, ubq->q_id, req->tag, io->flags,
 			ublk_get_iod(ubq, req->tag)->addr);
 
+	WARN_ON_ONCE(ublk_support_batch_io(ubq));
+
 	/*
 	 * Task is exiting if either:
 	 *
@@ -1816,6 +1823,8 @@ static void ublk_uring_cmd_cancel_fn(struct io_uring_cmd *cmd,
 	if (WARN_ON_ONCE(pdu->tag >= ubq->q_depth))
 		return;
 
+	WARN_ON_ONCE(ublk_support_batch_io(ubq));
+
 	task = io_uring_cmd_get_task(cmd);
 	io = &ubq->ios[pdu->tag];
 	if (WARN_ON_ONCE(task && task != io->task))
@@ -2127,7 +2136,10 @@ static int __ublk_fetch(struct io_uring_cmd *cmd, struct ublk_queue *ubq,
 	WARN_ON_ONCE(io->flags & UBLK_IO_FLAG_OWNED_BY_SRV);
 
 	ublk_fill_io_cmd(io, cmd, data);
-	WRITE_ONCE(io->task, get_task_struct(current));
+	if (ublk_support_batch_io(ubq))
+		WRITE_ONCE(io->task, NULL);
+	else
+		WRITE_ONCE(io->task, get_task_struct(current));
 	ublk_mark_io_ready(ub, ubq);
 out:
 	return ret;
@@ -2272,6 +2284,8 @@ static int __ublk_ch_uring_cmd(struct io_uring_cmd *cmd,
 
 	if (tag >= ubq->q_depth)
 		goto out;
+
+	WARN_ON_ONCE(ublk_support_batch_io(ubq));
 
 	io = &ubq->ios[tag];
 	task = READ_ONCE(io->task);
